795ce52509b3e5c4b2b527c252d8961c1a4abb3e
bluebayit7811 <bluebayitltd@gmail.com>
2021-11-04 13:35:04 +0600

    created qualification form

--- src/app/main/rams/QualificationsManagement/Qualifications/QualificationsTableHead.js
+++ src/app/main/rams/QualificationsManagement/Qualifications/QualificationsTableHead.js
@@ -1,20 +1,10 @@
 import Checkbox from '@material-ui/core/Checkbox';
-import Icon from '@material-ui/core/Icon';
-//import IconButton from '@material-ui/core/IconButton';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Tooltip from '@material-ui/core/Tooltip';
-import clsx from 'clsx';
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { removeQualifications } from '../store/qualificationsSlice';
+import React from 'react';
 
 const rows = [
     {
@@ -80,21 +70,9 @@ const QualificationsTableHead = (props) => {
 
     const numSelected = selectedQualificationIds.length;
 
-    const [selectedProductsMenu, setSelectedProductsMenu] = useState(null);
-
-    const dispatch = useDispatch();
-
     const createSortHandler = property => event => {
         props.onRequestSort(event, property);
     };
-
-    function openSelectedProductsMenu(event) {
-        setSelectedProductsMenu(event.currentTarget);
-    }
-
-    function closeSelectedProductsMenu() {
-        setSelectedProductsMenu(null);
-    }
 
     return (
         <TableHead>
@@ -106,40 +84,6 @@ const QualificationsTableHead = (props) => {
                         onChange={props.onSelectAllClick}
                     />
 
-                    <div
-                        className={clsx(
-                            'flex items-center justify-center absolute w-64 top-0 ltr:left-0 rtl:right-0 mx-56 h-64 z-10 border-b-1'
-                        )}
-                    >
-                        {/* <IconButton
-                            aria-owns={selectedProductsMenu ? 'selectedProductsMenu' : null}
-                            aria-haspopup="true"
-                            onClick={openSelectedProductsMenu}
-                        >
-                            <Icon>more_horiz</Icon>
-                        </IconButton> */}
-                        <Menu
-                            id="selectedProductsMenu"
-                            anchorEl={selectedProductsMenu}
-                            open={Boolean(selectedProductsMenu)}
-                            onClose={closeSelectedProductsMenu}
-                        >
-                            <MenuList>
-                                <MenuItem
-                                    onClick={() => {
-                                        dispatch(removeQualifications(selectedQualificationIds));
-                                        props.onMenuItemClick();
-                                        closeSelectedProductsMenu();
-                                    }}
-                                >
-                                    <ListItemIcon className="min-w-40">
-                                        <Icon>delete</Icon>
-                                    </ListItemIcon>
-                                    <ListItemText primary="Remove" />
-                                </MenuItem>
-                            </MenuList>
-                        </Menu>
-                    </div>
                 </TableCell>
                 {rows.map(row => {
                     return (
