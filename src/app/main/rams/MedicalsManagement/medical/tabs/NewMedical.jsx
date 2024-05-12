d2f346d5ed3839790ec5dfb5994ae50561a4d898
shuvo784 <mrmasudhossain@gmail.com>
2024-05-12 10:26:12 +0600

    update code medical section

--- src/app/main/rams/MedicalsManagement/medical/tabs/NewMedical.jsx
+++ src/app/main/rams/MedicalsManagement/medical/tabs/NewMedical.jsx
@@ -7,6 +7,12 @@ import { makeStyles } from '@mui/styles';
 
 import { Controller, FormProvider, useFormContext } from 'react-hook-form';
 import { useSelector } from 'react-redux';
+import { doneNotDone, medicalResults } from 'src/app/@data/data';
+import { GET_PASSENGER_BY_ID, MEDICAL_BY_PASSENGER_ID } from 'src/app/constant/constants';
+// import setIdIfValueIsObject from 'app/@helpers/setIdIfValueIsObject';
+import axios from 'axios';
+import history from '@history';
+import setIdIfValueIsObject from 'src/app/@helpers/setIdIfValueIsObject';
 
 const HtmlTooltip = styled(Tooltip)(({ theme }) => ({
 	[`& .${tooltipClasses.tooltip}`]: {
@@ -32,46 +38,27 @@ function NewMedical(props) {
 	const classes = useStyles(props);
 	const methods = useFormContext();
 	// const routeParams = useParams();
-	const { control, formState, getValues, setError } = methods;
+	const { control, formState, getValues, setError, reset, setValue } = methods;
 	const { errors } = formState;
 
 	const passengers = useSelector((state) => state.data.passengers);
 	const { fromSearch } = useParams();
 
+	const updateCurrentStatus = (id) => {
+		const authTOKEN = {
+			headers: {
+				'Content-type': 'application/json',
+				Authorization: localStorage.getItem('jwt_access_token')
+			}
+		};
+		axios.get(`${GET_PASSENGER_BY_ID}${id}`, authTOKEN).then((res) => {
+			setValue('current_status', res.data?.current_status?.id);
+		});
+	};
+
 	return (
 		<FormProvider>
 			<div className="flex justify-center w-full px-16 ">
-				{/* <Controller
-					name="passenger"
-					control={control}
-					render={({ field: { value } }) => (
-						<Autocomplete
-							className={`w-full max-w-320 h-48 ${classes.container}`}
-							freeSolo
-							autoHighlight
-							disabled={!!fromSearch}
-							value={value ? passengers.find((data) => data.id === value) : null}
-							options={passengers}
-							getOptionLabel={(option) =>
-								`${option.passenger_id} ${option.office_serial} ${option.passport_no} ${option.passenger_name}`
-							}
-							renderInput={(params) => (
-								<TextField
-									{...params}
-									className={classes.textField}
-									placeholder="Select Passenger"
-									label="Passenger"
-									required
-									helperText={errors?.passenger?.message}
-									variant="outlined"
-									autoFocus
-									InputLabelProps={value ? { shrink: true } : { style: { color: 'red' } }}
-								/>
-							)}
-						/>
-					)}
-				/> */}
-
 				<Controller
 					name="passenger"
 					control={control}
@@ -85,7 +72,51 @@ function NewMedical(props) {
 								`${option.passenger_id} ${option.office_serial} ${option.passport_no} ${option.passenger_name}`
 							}
 							onChange={(event, newValue) => {
-								onChange(newValue?.id);
+								updateCurrentStatus(newValue?.id);
+
+								if (newValue?.id) {
+									const authTOKEN = {
+										headers: {
+											'Content-type': 'application/json',
+											Authorization: localStorage.getItem('jwt_access_token')
+										}
+									};
+									axios
+										.get(`${MEDICAL_BY_PASSENGER_ID}${newValue?.id}`, authTOKEN)
+										.then((res) => {
+											if (res.data.id) {
+												reset({
+													...setIdIfValueIsObject(res.data),
+													passenger: newValue?.id
+												});
+												history.push(
+													`/apps/medical/medical/${newValue?.passenger_id || newValue?.id}`
+												);
+											} else {
+												history.push(`/apps/medical/medicals/new`);
+												reset({
+													passenger: newValue?.id,
+													medical_card: doneNotDone.find((data) => data.default)?.id,
+													medical_result: medicalResults.find((data) => data.default)?.id
+												});
+											}
+										})
+										.catch(() => {
+											history.push(`/apps/medical/medicals/new`);
+											reset({
+												passenger: newValue?.id,
+												medical_card: doneNotDone.find((data) => data.default)?.id,
+												medical_result: medicalResults.find((data) => data.default)?.id
+											});
+										});
+								} else {
+									history.push(`/apps/medical/medicals/new`);
+									reset({
+										passenger: newValue?.id,
+										medical_card: doneNotDone.find((data) => data.default)?.id,
+										medical_result: medicalResults.find((data) => data.default)?.id
+									});
+								}
 							}}
 							renderInput={(params) => (
 								<TextField
