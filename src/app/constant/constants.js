d809a1ee2743d340f8238b07ca58f23252678476
shuvo784 <mrmasudhossain@gmail.com>
2024-05-23 09:39:00 +0600

    work start embassys service

--- src/app/constant/constants.js
+++ src/app/constant/constants.js
@@ -2,7 +2,7 @@
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-export const BASE_URL = isProduction ? 'https://rl7811api.ramsbd.net' : 'http://192.168.0.169:8008';
+export const BASE_URL = isProduction ? 'https://rl7811api.ramsbd.net' : 'http://192.168.0.169:8005';
 export const CLIENT_URL = isProduction ? 'https://rl7811api.ramsbd.net' : 'http://192.168.0.119:3000';
 
 export const ADMIN_URL = isProduction ? 'https://rl7811api.ramsbd.net' : 'http://192.168.0.169:8008';
