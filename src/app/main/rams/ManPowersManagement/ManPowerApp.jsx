f710504f6d19ef137c5ff7fd3b7663edfa847a42
shuvo794 <shuvohossain794@gmail.com>
2024-05-28 11:29:18 +0600

    manPower config

--- src/app/main/rams/ManPowersManagement/ManPowerApp.jsx
+++ src/app/main/rams/ManPowersManagement/ManPowerApp.jsx
@@ -2,8 +2,8 @@ import { Outlet } from 'react-router';
 import withReducer from 'app/store/withReducer';
 import reducer from './store';
 
-function TrainingApp() {
+function ManPowerApp() {
 	return <Outlet />;
 }
 
-export default withReducer('trainingApp', reducer)(TrainingApp);
+export default withReducer('manPowerApp', reducer)(ManPowerApp);
