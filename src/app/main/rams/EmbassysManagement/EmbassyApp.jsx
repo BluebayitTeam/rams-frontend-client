8d3d3673cd09ffa19e5386a19c55e72ee3c704dd
shuvo784 <mrmasudhossain@gmail.com>
2024-05-30 15:45:15 +0600

    send update Emb

--- src/app/main/rams/EmbassysManagement/EmbassyApp.jsx
+++ src/app/main/rams/EmbassysManagement/EmbassyApp.jsx
@@ -2,8 +2,8 @@ import { Outlet } from 'react-router';
 import withReducer from 'app/store/withReducer';
 import reducer from './store';
 
-function MusanedOkalaApp() {
+function EmbassyApp() {
 	return <Outlet />;
 }
 
-export default withReducer('musanedOkalaApp', reducer)(MusanedOkalaApp);
+export default withReducer('embassyApp', reducer)(EmbassyApp);
