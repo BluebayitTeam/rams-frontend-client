6c284c081dec62e8dfea312853c6ff4ba7c28452
m-h-saikat <saikat.itbd@gmail.com>
2024-06-24 10:14:09 +0600

    Update CustomDatePicker

--- src/app/@components/CustomDatePicker.jsx
+++ src/app/@components/CustomDatePicker.jsx
@@ -7,38 +7,40 @@ import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import dayjs from 'dayjs';
 
 function CustomDatePicker(props) {
-	console.log('props', props);
-
 	return (
-		<LocalizationProvider dateAdapter={AdapterDayjs}>
-			<DatePicker
-				{...props?.field}
-				value={props?.field.value ? dayjs(props?.field.value, 'YYYY-MM-DD') : null}
-				onChange={(date) => {
-					props?.field.onChange(date ? dayjs(date).format('YYYY-MM-DD') : null);
-				}}
-				renderInput={(params) => (
-					<TextField
-						{...params}
-						className={props?.className || 'mt-8 mb-16 w-full'}
-						required={props?.required}
-						error={!!props?.error}
-						helperText={props?.helperText}
-						placeholder={props?.placeholder || 'DD-MM-YYYY'}
-						label="asdasdknl"
-						InputProps={{
-							startAdornment: (
-								<InputAdornment position="start">
-									<CalendarTodayIcon />
-								</InputAdornment>
-							)
-						}}
-						InputLabelProps={{ shrink: true }}
-					/>
-				)}
-				inputFormat="DD-MM-YYYY"
-			/>
-		</LocalizationProvider>
+		<div>
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+				<DatePicker
+					className="w-full"
+					{...props?.field}
+					value={props?.field.value ? dayjs(props?.field.value, 'YYYY-MM-DD') : null}
+					label={props?.label}
+					onChange={(date) => {
+						props?.field.onChange(date ? dayjs(date).format('YYYY-MM-DD') : null);
+					}}
+					renderInput={(params) => (
+						<TextField
+							{...params}
+							className={props?.className || 'mt-8 mb-16 w-full'}
+							required={props?.required}
+							error={!!props?.error}
+							helperText={props?.helperText}
+							placeholder={props?.placeholder || 'DD/MM/YYYY'}
+							InputProps={{
+								startAdornment: (
+									<InputAdornment position="start">
+										<CalendarTodayIcon />
+									</InputAdornment>
+								)
+							}}
+							InputLabelProps={{ shrink: true }}
+						/>
+					)}
+					inputFormat="DD/MM/YYYY"
+					format="DD/MM/YYYY"
+				/>
+			</LocalizationProvider>
+		</div>
 	);
 }
 
--- src/app/main/rams/AgentsManagement/agent/AgentForm.jsx
+++ src/app/main/rams/AgentsManagement/agent/AgentForm.jsx
@@ -37,6 +37,9 @@ function AgentForm(props) {
 	const { agentId } = routeParams;
 	const classes = useStyles(props);
 	const thanas = useSelector((state) => state.data.thanas);
+
+	console.log('thanas', thanas);
+
 	const cities = useSelector((state) => state.data.cities);
 	const countries = useSelector((state) => state.data.countries);
 	const groups = useSelector((state) => state.data.groups);
@@ -66,6 +69,19 @@ function AgentForm(props) {
 		const countryID = countries.find((data) => data.name === selectedCountry)?.id;
 		setValue('country', countryID);
 	};
+
+	// removed image
+	// const handleRemoveFile = () => {
+	// 	setPreviewImageFile(null);
+	// 	setFileExtName(null);
+	// 	setValue('image', '');
+
+	// 	if (fileInputRef.current) {
+	// 		fileInputRef.current.value = '';
+	// 	}
+
+	// 	console.log('sfsdferwer', getValues());
+	// };
 
 	return (
 		<div>
