c9306d42edb368ba14acedf9464f331c25c5e95c
shuvo794 <shuvohossain794@gmail.com>
2024-07-02 23:39:28 +0600

    send update code

--- src/app/constant/constants.js
+++ src/app/constant/constants.js
@@ -2,8 +2,10 @@
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-// export const BASE_URL = isProduction ? 'https://rl7811api.ramsbd.net' : 'http://192.168.0.117:8006';
-export const BASE_URL = 'https://rl7811api.ramsbd.net';
+export const BASE_URL = isProduction
+  ? 'https://rl7811api.ramsbd.net'
+  : 'http://192.168.0.117:8006';
+// export const BASE_URL = 'https://rl7811api.ramsbd.net';
 export const CLIENT_URL = isProduction
   ? 'https://rl7811api.ramsbd.net'
   : 'http://192.168.0.119:3000';
