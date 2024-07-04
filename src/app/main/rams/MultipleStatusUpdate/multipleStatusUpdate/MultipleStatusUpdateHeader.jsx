e5c198a05905280787243e03de2e74d64210712e
shuvo784 <mrmasudhossain@gmail.com>
2024-07-04 10:01:55 +0600

    work a header file

--- src/app/main/rams/MultipleStatusUpdate/multipleStatusUpdate/MultipleStatusUpdateHeader.jsx
+++ src/app/main/rams/MultipleStatusUpdate/multipleStatusUpdate/MultipleStatusUpdateHeader.jsx
@@ -3,9 +3,10 @@ import { useTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import { motion } from 'framer-motion';
 import { useFormContext } from 'react-hook-form';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { CustomNotification } from 'src/app/@customHooks/notificationAlert';
+import _ from '@lodash';
 import { useCreateMultipleStatusUpdateMutation } from '../MultipleStatusUpdatesApi';
 
 /**
@@ -19,50 +20,100 @@ function MultipleStatusUpdateHeader({ handleReset }) {
 	const { formState, watch, getValues } = methods;
 	const { isValid, dirtyFields } = formState;
 	const theme = useTheme();
-	const navigate = useNavigate();
+
 	const { checkbox, passenger, email } = watch();
 	const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+	const name = watch('name');
 
 	useEffect(() => {
 		setIsButtonDisabled(!(checkbox && passenger && email));
 	}, [checkbox, passenger, email]);
 
-	function handleSaveDocumentSend() {
+	function handleSaveMultipleStatusUpdate() {
 		saveDocumentSend(getValues()).then((res) => {
 			CustomNotification('success', 'Multiple Status UpdateSuccessfully..');
 			handleReset({ email: '' });
 		});
 	}
 
+	function handleCancelMultipleStatusUpdate() {
+		handleReset({});
+		// dispatch(resetMultipleStatusUpdateTable());
+
+		handleReset({ date: '' });
+	}
+
 	return (
 		<div className="flex flex-col sm:flex-row flex-1 w-full items-center justify-between space-y-8 sm:space-y-0 py-24 sm:py-32 px-24 md:px-32">
 			<div className="flex flex-col items-start space-y-8 sm:space-y-0 w-full sm:max-w-full min-w-0">
-				<div className="flex items-center max-w-full">
+				<div className="flex flex-col items-start max-w-full min-w-0">
 					<motion.div
-						className="flex flex-col min-w-0 mx-8 sm:mx-16"
-						initial={{ x: -20 }}
-						animate={{ x: 0, transition: { delay: 0.3 } }}
+						initial={{ x: 20, opacity: 0 }}
+						animate={{ x: 0, opacity: 1, transition: { delay: 0.3 } }}
 					>
-						<Typography className="text-16 sm:text-20 truncate font-semibold">
-							Multiple Status Update
+						<Typography
+							className="flex items-center sm:mb-2"
+							component={Link}
+							role="button"
+							to="/apps/multipleStatusUpdate-management/multipleStatusUpdates"
+							color="inherit"
+						>
+							{/* <Icon className="text-20">
+								{theme.direction === 'ltr' ? 'arrow_back' : 'arrow_forward'}
+							</Icon> */}
 						</Typography>
 					</motion.div>
+
+					<div className="flex items-center max-w-full">
+						<motion.div
+							className="hidden sm:flex"
+							initial={{ scale: 0 }}
+							animate={{ scale: 1, transition: { delay: 0.3 } }}
+						/>
+
+						<div className="flex flex-col min-w-0 mx-8 sm:mc-16">
+							<motion.div
+								initial={{ x: -20 }}
+								animate={{ x: 0, transition: { delay: 0.3 } }}
+							>
+								<span className="hidden sm:flex mx-4 font-medium">Multiple Status Update</span>
+								<Typography className="text-16 sm:text-20 truncate font-semibold">
+									{name || 'Create Multiple Status Update'}
+								</Typography>
+								<Typography
+									variant="caption"
+									className="font-medium"
+								>
+									Multiple Status Update Detail
+								</Typography>
+							</motion.div>
+						</div>
+					</div>
 				</div>
 			</div>
 
 			<motion.div
 				className="flex"
 				initial={{ opacity: 0, x: 20 }}
-				animate={{ opacity: 1, x: 0, transition: { delay: 0.2 } }}
+				animate={{ opacity: 1, x: 0, transition: { delay: 0.3 } }}
 			>
 				<Button
 					className="whitespace-nowrap mx-4"
 					variant="contained"
 					color="secondary"
-					onClick={handleSaveDocumentSend}
-					disabled={isButtonDisabled}
+					disabled={_.isEmpty(dirtyFields) || !isValid}
+					onClick={handleSaveMultipleStatusUpdate}
 				>
-					Send
+					Save
+				</Button>
+
+				<Button
+					className="whitespace-nowrap mx-4"
+					variant="contained"
+					style={{ backgroundColor: '#FFAA4C', color: 'white' }}
+					onClick={handleCancelMultipleStatusUpdate}
+				>
+					Cancel
 				</Button>
 			</motion.div>
 		</div>
