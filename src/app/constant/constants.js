73a8de254b5050fcf7f85da9a36058545b099637
shuvo794 <shuvohossain794@gmail.com>
2024-07-08 22:40:30 +0600

    chenge_test

--- src/app/constant/constants.js
+++ src/app/constant/constants.js
@@ -6,6 +6,8 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 // added by live api
 
+// test git
+
 export const BASE_URL = isProduction
   ? 'https://rl7811api.ramsbd.net'
   : 'http://92.204.172.226:8000';
