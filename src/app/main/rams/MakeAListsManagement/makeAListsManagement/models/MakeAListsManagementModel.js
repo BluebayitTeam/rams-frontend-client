d796eb09a06b4e07feffe9ebb694ce493fd43462
shuvo784 <mrmasudhossain@gmail.com>
2024-07-10 11:57:11 +0600

    send update code

--- src/app/main/rams/MakeAListsManagement/makeAListsManagement/models/MakeAListsManagementModel.js
+++ src/app/main/rams/MakeAListsManagement/makeAListsManagement/models/MakeAListsManagementModel.js
@@ -4,7 +4,7 @@ import moment from 'moment';
 const MakeAListsManagementModel = (data) =>
 	_.defaults(data || {}, {
 		make_date: moment(new Date()).format('YYYY-MM-DD'),
-		id: _.uniqueId('department-'),
+		id: _.uniqueId('MakeAListsManagement-'),
 		name: ''
 	});
 export default MakeAListsManagementModel;
