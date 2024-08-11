e53c27e428ecad79c8a934669c8fd9d6a0a09f0c
m-h-saikat <saikat.itbd@gmail.com>
2024-08-11 15:19:47 +0600

    update

---
+++ src/app/main/rams/AgentReportManagement/demandAssign/DemandAssignHeader.jsx
@@ -0,0 +1,81 @@
+import Button from '@mui/material/Button';
+import { useTheme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import { motion } from 'framer-motion';
+import { useFormContext } from 'react-hook-form';
+import { useNavigate, useParams } from 'react-router-dom';
+import { AddedSuccessfully } from 'src/app/@customHooks/notificationAlert';
+import { useCreateDemandAssignMutation } from '../DemandAssignsApi';
+
+/**
+ * The demandAssign header.
+ */
+function DemandAssignHeader({ handleReset }) {
+	const routeParams = useParams();
+	const { demandAssignId } = routeParams;
+	const [createDemandAssign] = useCreateDemandAssignMutation();
+	const methods = useFormContext();
+	const { formState, watch, getValues, reset } = methods;
+	const { isValid, dirtyFields } = formState;
+	const theme = useTheme();
+	const navigate = useNavigate();
+	const { name, images, featuredImageId } = watch();
+	const handleDelete = localStorage.getItem('deleteDemandAssign');
+	const handleUpdate = localStorage.getItem('updateDemandAssign');
+
+	function handleCreateDemandAssign() {
+		createDemandAssign(getValues())
+			.unwrap()
+			.then((data) => {
+				AddedSuccessfully();
+				handleReset();
+				navigate(`/apps/demandAssign/demandAssigns`);
+			});
+	}
+
+	function handleCancel() {
+		handleReset();
+		navigate(`/apps/demandAssign/demandAssigns`);
+	}
+
+	return (
+		<div className="flex flex-col sm:flex-row flex-1 w-full items-center justify-between space-y-8 sm:space-y-0 py-24 sm:py-32 px-24 md:px-32">
+			<div className="flex flex-col items-start space-y-8 sm:space-y-0 w-full sm:max-w-full min-w-0">
+				<div className="flex items-center max-w-full">
+					<motion.div
+						className="flex flex-col min-w-0 mx-8 sm:mx-16"
+						initial={{ x: -20 }}
+						animate={{ x: 0, transition: { delay: 0.3 } }}
+					>
+						<Typography className="text-16 sm:text-20 truncate font-semibold">Demand Assign</Typography>
+					</motion.div>
+				</div>
+			</div>
+
+			<motion.div
+				className="flex"
+				initial={{ opacity: 0, x: 20 }}
+				animate={{ opacity: 1, x: 0, transition: { delay: 0.2 } }}
+			>
+				<Button
+					className="whitespace-nowrap mx-4 "
+					variant="contained"
+					color="secondary"
+					onClick={handleCreateDemandAssign}
+				>
+					Save
+				</Button>
+
+				<Button
+					className="whitespace-nowrap mx-4 text-white bg-orange-500 hover:bg-orange-800 active:bg-orange-700 focus:outline-none focus:ring focus:ring-orange-300"
+					variant="contained"
+					onClick={handleCancel}
+				>
+					Cancel
+				</Button>
+			</motion.div>
+		</div>
+	);
+}
+
+export default DemandAssignHeader;
