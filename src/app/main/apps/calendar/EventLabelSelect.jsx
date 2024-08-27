45d8b4c83c9617148e0cf0dec610de139b3c6919
shuvo784 <mrmasudhossain@gmail.com>
2024-08-27 17:19:05 +0600

    send update code

--- src/app/main/apps/calendar/EventLabelSelect.jsx
+++ src/app/main/apps/calendar/EventLabelSelect.jsx
@@ -5,6 +5,7 @@ import Select from '@mui/material/Select';
 import { forwardRef, useState } from 'react';
 import Box from '@mui/material/Box';
 import { useGetCalendarLabelsQuery } from './CalendarApi';
+
 /**
  * The event label select.
  */
@@ -12,12 +13,13 @@ const EventLabelSelect = forwardRef((props, ref) => {
 	const { value, onChange, className, searchKey } = props;
 	const [pageSize, setPageSize] = useState({ page: 1, size: 25 });
 
-	const { data: labels } = useGetCalendarLabelsQuery({ ...pageSize, searchKey });
-	console.log('dfjgdjfghdjfkghkdhkdfhkdfhdkfh', labels);
+	const { data: labels = [] } = useGetCalendarLabelsQuery({ ...pageSize, searchKey });
+	console.log('Labels data:', labels);
 
 	const handleChange = (event) => {
 		onChange(event.target.value);
 	};
+
 	return (
 		<FormControl
 			fullWidth
@@ -33,21 +35,23 @@ const EventLabelSelect = forwardRef((props, ref) => {
 				ref={ref}
 				classes={{ select: 'flex items-center space-x-12' }}
 			>
-				{labels?.map((label) => (
-					<MenuItem
-						value={label.id}
-						key={label.id}
-						className="space-x-12"
-					>
-						<Box
-							className="w-12 h-12 shrink-0 rounded-full"
-							sx={{ backgroundColor: label.color }}
-						/>
-						<span>{label.title}</span>
-					</MenuItem>
-				))}
+				{Array.isArray(labels) &&
+					labels.map((label) => (
+						<MenuItem
+							value={label.id}
+							key={label.id}
+							className="space-x-12"
+						>
+							<Box
+								className="w-12 h-12 shrink-0 rounded-full"
+								sx={{ backgroundColor: label.color }}
+							/>
+							<span>{label.title}</span>
+						</MenuItem>
+					))}
 			</Select>
 		</FormControl>
 	);
 });
+
 export default EventLabelSelect;
