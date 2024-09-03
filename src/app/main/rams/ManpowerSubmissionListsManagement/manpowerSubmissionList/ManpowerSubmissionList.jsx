e693bc9505112b138a38c6606062d77f828d4462
shuvo784 <mrmasudhossain@gmail.com>
2024-09-04 01:16:55 +0600

    send update code

--- src/app/main/rams/ManpowerSubmissionListsManagement/manpowerSubmissionList/ManpowerSubmissionList.jsx
+++ src/app/main/rams/ManpowerSubmissionListsManagement/manpowerSubmissionList/ManpowerSubmissionList.jsx
@@ -1,157 +1,192 @@
-import FuseLoading from '@fuse/core/FuseLoading';
-import FusePageCarded from '@fuse/core/FusePageCarded';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { FormProvider, useForm } from 'react-hook-form';
-import useThemeMediaQuery from '@fuse/hooks/useThemeMediaQuery';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { z } from 'zod';
-import ManpowerSubmissionListHeader from './ManpowerSubmissionListHeader';
-import ManpowerSubmissionListModel from './models/ManpowerSubmissionListModel';
+import FuseLoading from "@fuse/core/FuseLoading";
+import FusePageCarded from "@fuse/core/FusePageCarded";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { FormProvider, useForm } from "react-hook-form";
+import useThemeMediaQuery from "@fuse/hooks/useThemeMediaQuery";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import ManpowerSubmissionListHeader from "./ManpowerSubmissionListHeader";
+import ManpowerSubmissionListModel from "./models/ManpowerSubmissionListModel";
 import {
-	selectFilteredManpowerSubmissionLists,
-	useGetManpowerSubmissionListQuery,
-	useGetManpowerSubmissionListsQuery
-} from '../ManpowerSubmissionListsApi';
-import ManpowerSubmissionListForm from './ManpowerSubmissionListForm';
-import ManpowerSubmissionLists from '../manpowerSubmissionLists/ManpowerSubmissionLists';
+  selectFilteredManpowerSubmissionLists,
+  useCreateManpowerSubmissionListMutation,
+  useGetManpowerSubmissionListQuery,
+  useGetManpowerSubmissionListsQuery,
+} from "../ManpowerSubmissionListsApi";
+import ManpowerSubmissionListForm from "./ManpowerSubmissionListForm";
+import ManpowerSubmissionLists from "../manpowerSubmissionLists/ManpowerSubmissionLists";
+import { AddedSuccessfully } from "src/app/@customHooks/notificationAlert";
 /**
  * Form Validation Schema
  */
 const schema = z.object({
-	first_name: z
-		.string()
-		.nonempty('You must enter a manpowerSubmissionList name')
-		.min(5, 'The manpowerSubmissionList name must be at least 5 characters')
+  first_name: z
+    .string()
+    .nonempty("You must enter a manpowerSubmissionList name")
+    .min(5, "The manpowerSubmissionList name must be at least 5 characters"),
 });
 
 function ManpowerSubmissionList(props) {
-	const isMobile = useThemeMediaQuery((theme) => theme.breakpoints.down('lg'));
-	const routeParams = useParams();
-	const { manpowerSubmissionListId } = routeParams;
+  const isMobile = useThemeMediaQuery((theme) => theme.breakpoints.down("lg"));
+  const routeParams = useParams();
+  const { manpowerSubmissionListId } = routeParams;
+  const [tabileShow, setTabileShow] = useState(false);
+  console.log("tabileShow", tabileShow);
 
-	const {
-		data: manpowerSubmissionList,
-		isLoading,
-		isError
-	} = useGetManpowerSubmissionListQuery(manpowerSubmissionListId, {
-		skip: !manpowerSubmissionListId || manpowerSubmissionListId === 'new'
-	});
+  const {
+    data: manpowerSubmissionList,
+    isLoading,
+    isError,
+  } = useGetManpowerSubmissionListQuery(manpowerSubmissionListId, {
+    skip: !manpowerSubmissionListId || manpowerSubmissionListId === "new",
+  });
 
-	const [tabValue, setTabValue] = useState(0);
-	const methods = useForm({
-		mode: 'onChange',
-		defaultValues: {
-			man_power_date: ''
-		},
-		resolver: zodResolver(schema)
-	});
-	const { reset, watch } = methods;
-	const form = watch();
+  const [tabValue, setTabValue] = useState(0);
+  const methods = useForm({
+    mode: "onChange",
+    defaultValues: {
+      man_power_date: "",
+    },
+    resolver: zodResolver(schema),
+  });
+  const { reset, watch, getValues } = methods;
+  const form = watch();
 
-	const passenger = watch('passenger');
-	const manPowerDate = watch('man_power_date');
+  const passenger = watch("passenger");
+  const manPowerDate = watch("man_power_date");
 
-	const { data } = useGetManpowerSubmissionListsQuery({
-		passenger,
-		manPowerDate
-	});
+  const navigate = useNavigate();
 
-	function handleSearchPassengerClick() {
-		const selectedPassenger = watch('passenger');
-		console.log('selectedPassenger', selectedPassenger);
+  const { data } = useGetManpowerSubmissionListsQuery({
+    passenger,
+    manPowerDate,
+  });
 
-		if (selectedPassenger) {
-			selectFilteredManpowerSubmissionLists({ passenger: selectedPassenger.passenger_id });
-		}
-	}
+  const [createManpowerSubmissionList] =
+    useCreateManpowerSubmissionListMutation();
 
-	function handleSearchManPowerDateClick() {
-		const selectedManPowerDate = watch('man_power_date');
-		console.log('selectedDate', selectedManPowerDate);
+  function handleCreateManpowerSubmissionList() {
+    createManpowerSubmissionList(getValues())
+      .unwrap()
+      .then((data) => {
+        if (data) {
+          AddedSuccessfully();
+        }
+        setTabileShow(true);
 
-		if (selectedManPowerDate) {
-			selectFilteredManpowerSubmissionLists({ manPowerDate: selectedManPowerDate });
-		}
-	}
+        navigate(`/apps/manpowerSubmissionList/manpowerSubmissionLists/new`);
+      })
+      .catch((error) => {
+        CustomNotification("errorhfghgfhg", `${error.response.data.passenger}`);
+      });
+  }
 
-	console.log('fdksdfhsdkfhsdkf', data);
-	useEffect(() => {
-		if (manpowerSubmissionListId === 'new') {
-			reset(ManpowerSubmissionListModel({}));
-		}
-	}, [manpowerSubmissionListId, reset]);
+  function handleSearchPassengerClick() {
+    const selectedPassenger = watch("passenger");
+    console.log("selectedPassenger", selectedPassenger);
 
-	useEffect(() => {
-		if (manpowerSubmissionList) {
-			reset({ ...manpowerSubmissionList });
-		}
-	}, [manpowerSubmissionList, reset, manpowerSubmissionList?.id]);
+    if (selectedPassenger) {
+      selectFilteredManpowerSubmissionLists({
+        passenger: selectedPassenger.passenger_id,
+      });
+      setTabileShow(true);
+    } else {
+      setTabileShow(false);
+    }
+  }
 
-	function handleTabChange(event, value) {
-		setTabValue(value);
-	}
+  // For debugging purposes
 
-	if (isLoading) {
-		return <FuseLoading />;
-	}
+  function handleSearchManPowerDateClick() {
+    const selectedManPowerDate = watch("man_power_date");
+    console.log("selectedDate", selectedManPowerDate);
 
-	/**
-	 * Show Message if the requested manpowerSubmissionLists is not exists
-	 */
-	if (isError && manpowerSubmissionListId !== 'new') {
-		return (
-			<motion.div
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1, transition: { delay: 0.1 } }}
-				className="flex flex-col flex-1 items-center justify-center h-full"
-			>
-				<Typography
-					color="text.secondary"
-					variant="h5"
-				>
-					There is no such manpowerSubmissionList!
-				</Typography>
-				<Button
-					className="mt-24"
-					component={Link}
-					variant="outlined"
-					to="/apps/manpowerSubmissionList/manpowerSubmissionLists"
-					color="inherit"
-				>
-					Go to ManpowerSubmissionLists Page
-				</Button>
-			</motion.div>
-		);
-	}
+    if (selectedManPowerDate) {
+      selectFilteredManpowerSubmissionLists({
+        manPowerDate: selectedManPowerDate,
+      });
+      setTabileShow(true);
+    }
+  }
 
-	return (
-		<FormProvider {...methods}>
-			<FusePageCarded
-				header={<ManpowerSubmissionListHeader />}
-				content={
-					<div className="p-16 ">
-						<ManpowerSubmissionListForm
-							manpowerSubmissionListId={manpowerSubmissionListId}
-							handleSearchPassengerClick={handleSearchPassengerClick}
-							handleSearchManPowerDateClick={handleSearchManPowerDateClick}
-						/>
-						<br />
-						<br />
-						<br />
-						<br />
-						<br />
-						<br />
-						<ManpowerSubmissionLists data={data} />
-					</div>
-				}
-				scroll={isMobile ? 'normal' : 'content'}
-			/>
-		</FormProvider>
-	);
+  console.log("fdksdfhsdkfhsdkf", data);
+  useEffect(() => {
+    if (manpowerSubmissionListId === "new") {
+      reset(ManpowerSubmissionListModel({}));
+    }
+  }, [manpowerSubmissionListId, reset]);
+
+  useEffect(() => {
+    if (manpowerSubmissionList) {
+      reset({ ...manpowerSubmissionList });
+    }
+  }, [manpowerSubmissionList, reset, manpowerSubmissionList?.id]);
+
+  function handleTabChange(event, value) {
+    setTabValue(value);
+  }
+
+  if (isLoading) {
+    return <FuseLoading />;
+  }
+
+  /**
+   * Show Message if the requested manpowerSubmissionLists is not exists
+   */
+  if (isError && manpowerSubmissionListId !== "new") {
+    return (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { delay: 0.1 } }}
+        className="flex flex-col flex-1 items-center justify-center h-full"
+      >
+        <Typography color="text.secondary" variant="h5">
+          There is no such manpowerSubmissionList!
+        </Typography>
+        <Button
+          className="mt-24"
+          component={Link}
+          variant="outlined"
+          to="/apps/manpowerSubmissionList/manpowerSubmissionLists"
+          color="inherit"
+        >
+          Go to ManpowerSubmissionLists Page
+        </Button>
+      </motion.div>
+    );
+  }
+
+  return (
+    <FormProvider {...methods}>
+      <FusePageCarded
+        header={<ManpowerSubmissionListHeader />}
+        content={
+          <div className="p-16 ">
+            <ManpowerSubmissionListForm
+              manpowerSubmissionListId={manpowerSubmissionListId}
+              handleSearchPassengerClick={handleSearchPassengerClick}
+              handleSearchManPowerDateClick={handleSearchManPowerDateClick}
+              handleCreateManpowerSubmissionList={
+                handleCreateManpowerSubmissionList
+              }
+            />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <ManpowerSubmissionLists data={data} tabileShow={tabileShow} />
+          </div>
+        }
+        scroll={isMobile ? "normal" : "content"}
+      />
+    </FormProvider>
+  );
 }
 
 export default ManpowerSubmissionList;
--- src/app/main/rams/ManpowerSubmissionListsManagement/manpowerSubmissionList/ManpowerSubmissionListForm.jsx
+++ src/app/main/rams/ManpowerSubmissionListsManagement/manpowerSubmissionList/ManpowerSubmissionListForm.jsx
@@ -1,172 +1,184 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable no-undef */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import { getAgencys, getCountries, getCurrentStatuss, getPassengers } from 'app/store/dataSlice';
-import { useEffect } from 'react';
-import { useFormContext } from 'react-hook-form';
-import { useSelector, useDispatch } from 'react-redux';
-import CustomDropdownField from 'src/app/@components/CustomDropdownField';
-import { makeStyles } from '@mui/styles';
-import { Search } from '@mui/icons-material';
-import { Button } from '@mui/material';
-import CustomDatePicker from 'src/app/@components/CustomDatePicker';
-import { AddedSuccessfully, CustomNotification } from 'src/app/@customHooks/notificationAlert';
-import { useNavigate } from 'react-router';
-import { GET_FORM_CONTENT_DETAILS_BY_TITLE } from 'src/app/constant/constants';
-import { MANPOWER_SUBMISSION_LIST_FOOTER } from 'src/app/constant/FormContentTitle/formContentTitle';
-import { useCreateManpowerSubmissionListMutation } from '../ManpowerSubmissionListsApi';
+import {
+  getAgencys,
+  getCountries,
+  getCurrentStatuss,
+  getPassengers,
+} from "app/store/dataSlice";
+import { useEffect } from "react";
+import { useFormContext } from "react-hook-form";
+import { useSelector, useDispatch } from "react-redux";
+import CustomDropdownField from "src/app/@components/CustomDropdownField";
+import { makeStyles } from "@mui/styles";
+import { Search } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import CustomDatePicker from "src/app/@components/CustomDatePicker";
+import {
+  AddedSuccessfully,
+  CustomNotification,
+} from "src/app/@customHooks/notificationAlert";
+import { useNavigate } from "react-router";
+import { GET_FORM_CONTENT_DETAILS_BY_TITLE } from "src/app/constant/constants";
+import { MANPOWER_SUBMISSION_LIST_FOOTER } from "src/app/constant/FormContentTitle/formContentTitle";
+import { useCreateManpowerSubmissionListMutation } from "../ManpowerSubmissionListsApi";
 
 const useStyles = makeStyles((theme) => ({
-	searchContainer: ({ isPassenger }) => ({
-		color: theme.palette.primary.main,
-		background: 'transparent',
-		borderColor: theme.palette.primary.main,
-		cursor: isPassenger && 'pointer',
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center',
-		border: '1px solid',
-		height: '52px',
-		width: '52px',
-		marginTop: '8px',
-		borderRadius: '5px',
-		'&:hover': {
-			color: isPassenger ? theme.palette.primary.dark : theme.palette.primary.main,
-			background: isPassenger ? theme.palette.primary.light : 'transparent',
-			borderColor: isPassenger ? theme.palette.primary.dark : theme.palette.primary.main
-		},
-		'&:active': {
-			color: isPassenger ? theme.palette.primary.light : theme.palette.primary.main,
-			background: isPassenger ? theme.palette.primary.dark : 'transparent',
-			borderColor: isPassenger ? theme.palette.primary.light : theme.palette.primary.main
-		}
-	})
+  searchContainer: ({ isPassenger }) => ({
+    color: theme.palette.primary.main,
+    background: "transparent",
+    borderColor: theme.palette.primary.main,
+    cursor: isPassenger && "pointer",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "1px solid",
+    height: "52px",
+    width: "52px",
+    marginTop: "8px",
+    borderRadius: "5px",
+    "&:hover": {
+      color: isPassenger
+        ? theme.palette.primary.dark
+        : theme.palette.primary.main,
+      background: isPassenger ? theme.palette.primary.light : "transparent",
+      borderColor: isPassenger
+        ? theme.palette.primary.dark
+        : theme.palette.primary.main,
+    },
+    "&:active": {
+      color: isPassenger
+        ? theme.palette.primary.light
+        : theme.palette.primary.main,
+      background: isPassenger ? theme.palette.primary.dark : "transparent",
+      borderColor: isPassenger
+        ? theme.palette.primary.light
+        : theme.palette.primary.main,
+    },
+  }),
 }));
 
-function ManpowerSubmissionListForm({ handleSearchPassengerClick, handleSearchManPowerDateClick }) {
-	const dispatch = useDispatch();
-	const methods = useFormContext();
-	const { formState, watch, getValues, reset } = methods;
+function ManpowerSubmissionListForm({
+  handleSearchPassengerClick,
+  handleSearchManPowerDateClick,
+  handleCreateManpowerSubmissionList,
+}) {
+  const dispatch = useDispatch();
+  const methods = useFormContext();
+  const { formState, watch, getValues, reset } = methods;
 
-	const { errors } = formState;
-	const { agencies, countries, passengers } = useSelector((state) => state.data);
-	const [createManpowerSubmissionList] = useCreateManpowerSubmissionListMutation();
-	const navigate = useNavigate();
-	const classes = useStyles({ isPassenger: watch('passenger') });
+  const { errors } = formState;
+  const { agencies, countries, passengers } = useSelector(
+    (state) => state.data
+  );
 
-	useEffect(() => {
-		dispatch(getPassengers());
-		dispatch(getAgencys());
-		dispatch(getCountries());
-		dispatch(getCurrentStatuss());
-	}, []);
+  const classes = useStyles({ isPassenger: watch("passenger") });
 
-	function handleCreateManpowerSubmissionList() {
-		createManpowerSubmissionList(getValues())
-			.unwrap()
-			.then((data) => {
-				if (data) {
-					AddedSuccessfully();
-				}
+  useEffect(() => {
+    dispatch(getPassengers());
+    dispatch(getAgencys());
+    dispatch(getCountries());
+    dispatch(getCurrentStatuss());
+  }, []);
 
-				navigate(`/apps/manpowerSubmissionList/manpowerSubmissionLists/new`);
-			})
-			.catch((error) => {
-				CustomNotification('error', `${error.response.data.passenger}`);
-			});
-	}
+  function handleCancel() {
+    reset({});
+  }
 
-	function handleCancel() {
-		reset({});
-	}
+  useEffect(() => {
+    const authTOKEN = {
+      headers: {
+        "Content-type": "application/json",
+        Authorization: localStorage.getItem("jwt_access_token"),
+      },
+    };
 
-	useEffect(() => {
-		const authTOKEN = {
-			headers: {
-				'Content-type': 'application/json',
-				Authorization: localStorage.getItem('jwt_access_token')
-			}
-		};
+    fetch(
+      `${GET_FORM_CONTENT_DETAILS_BY_TITLE}${MANPOWER_SUBMISSION_LIST_FOOTER}`,
+      authTOKEN
+    )
+      .then((response) => response.json())
+      .then((data) =>
+        sessionStorage.setItem(
+          "formContentFooterData",
+          data?.formcontent_detail[0]?.details || ""
+        )
+      );
+  }, []);
 
-		fetch(`${GET_FORM_CONTENT_DETAILS_BY_TITLE}${MANPOWER_SUBMISSION_LIST_FOOTER}`, authTOKEN)
-			.then((response) => response.json())
-			.then((data) =>
-				sessionStorage.setItem('formContentFooterData', data?.formcontent_detail[0]?.details || '')
-			);
-	}, []);
+  return (
+    <div>
+      <CustomDropdownField
+        name="agency"
+        label="Agency"
+        options={agencies}
+        optionLabelFormat={(option) => `${option?.name}`}
+        // onChange={(newValue) => setValue('agency_info', newValue)}
+      />
+      <CustomDropdownField
+        name="country"
+        label="Country"
+        options={countries}
+        optionLabelFormat={(option) => `${option?.name}`}
+      />
 
-	return (
-		<div>
-			<CustomDropdownField
-				name="agency"
-				label="Agency"
-				options={agencies}
-				optionLabelFormat={(option) => `${option?.name}`}
-				// onChange={(newValue) => setValue('agency_info', newValue)}
-			/>
-			<CustomDropdownField
-				name="country"
-				label="Country"
-				options={countries}
-				optionLabelFormat={(option) => `${option?.name}`}
-			/>
+      <div className="flex flex-nowrap">
+        <div className="w-full">
+          <CustomDropdownField
+            name="passenger"
+            label="Passenger"
+            options={passengers}
+            optionLabelFormat={(option) =>
+              `${option.passenger_id} ${option.office_serial} ${option.passport_no} ${option.passenger_name}`
+            }
+          />
+        </div>
+        <div
+          className={classes.searchContainer}
+          onClick={handleSearchPassengerClick} // Call the handleSearchPassengerClick function on click
+        >
+          <Search />
+        </div>
+      </div>
 
-			<div className="flex flex-nowrap">
-				<div className="w-full">
-					<CustomDropdownField
-						name="passenger"
-						label="Passenger"
-						options={passengers}
-						optionLabelFormat={(option) =>
-							`${option.passenger_id} ${option.office_serial} ${option.passport_no} ${option.passenger_name}`
-						}
-					/>
-				</div>
-				<div
-					className={classes.searchContainer}
-					onClick={handleSearchPassengerClick} // Call the handleSearchPassengerClick function on click
-				>
-					<Search />
-				</div>
-			</div>
+      <div className="flex flex-nowrap">
+        <div className="w-full">
+          <CustomDatePicker
+            name="man_power_date"
+            label="Manpower Date"
+            required
+            placeholder="DD-MM-YYYY"
+          />
+        </div>
+        <div
+          className={classes.searchContainer}
+          onClick={handleSearchManPowerDateClick}
+        >
+          <Search />
+        </div>
+      </div>
 
-			<div className="flex flex-nowrap">
-				<div className="w-full">
-					<CustomDatePicker
-						name="man_power_date"
-						label="Manpower Date"
-						required
-						placeholder="DD-MM-YYYY"
-					/>
-				</div>
-				<div
-					className={classes.searchContainer}
-					onClick={handleSearchManPowerDateClick}
-				>
-					<Search />
-				</div>
-			</div>
+      <Button
+        className="whitespace-nowrap mx-4"
+        variant="contained"
+        color="secondary"
+        // disabled={_.isEmpty(dirtyFields) || !isValid}
+        onClick={handleCreateManpowerSubmissionList}
+      >
+        Save
+      </Button>
 
-			<Button
-				className="whitespace-nowrap mx-4"
-				variant="contained"
-				color="secondary"
-				// disabled={_.isEmpty(dirtyFields) || !isValid}
-				onClick={handleCreateManpowerSubmissionList}
-			>
-				Save
-			</Button>
-
-			<Button
-				className="whitespace-nowrap mx-4"
-				variant="contained"
-				style={{ backgroundColor: '#FFAA4C', color: 'white' }}
-				onClick={handleCancel}
-			>
-				Cancel
-			</Button>
-		</div>
-	);
+      <Button
+        className="whitespace-nowrap mx-4"
+        variant="contained"
+        style={{ backgroundColor: "#FFAA4C", color: "white" }}
+        onClick={handleCancel}
+      >
+        Cancel
+      </Button>
+    </div>
+  );
 }
 
 export default ManpowerSubmissionListForm;
