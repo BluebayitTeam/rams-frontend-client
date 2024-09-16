b7a8790d0c48ef5fa85a061ecd01dcbcfb75b3f9
shuvo784 <mrmasudhossain@gmail.com>
2024-09-16 21:59:27 +0600

    send  update code

--- src/app/main/rams/AgentReportManagement/agentReport/AgentFilterMenu.jsx
+++ src/app/main/rams/AgentReportManagement/agentReport/AgentFilterMenu.jsx
@@ -1,13 +1,13 @@
 import { useTheme } from '@mui/material/styles';
-import { useFormContext } from 'react-hook-form';
-import ReportTextField from 'src/app/@components/ReportComponents/ReportTextField';
-import { useEffect, useRef, useState } from 'react';
 import { makeStyles } from '@mui/styles';
-import { useSelector, useDispatch } from 'react-redux';
-import ReportSelect from 'src/app/@components/ReportComponents/ReportSelect';
 import { getCities, getGroups } from 'app/store/dataSlice';
-import ReportDatePicker from 'src/app/@components/ReportComponents/ReportDatePicker';
+import { useEffect, useRef, useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
 import Keyword from 'src/app/@components/ReportComponents/Keyword';
+import ReportDatePicker from 'src/app/@components/ReportComponents/ReportDatePicker';
+import ReportSelect from 'src/app/@components/ReportComponents/ReportSelect';
+import ReportTextField from 'src/app/@components/ReportComponents/ReportTextField';
 import { getReportFilterMakeStyles } from '../../ReportUtilities/reportMakeStyls';
 
 const useStyles = makeStyles((theme) => ({
