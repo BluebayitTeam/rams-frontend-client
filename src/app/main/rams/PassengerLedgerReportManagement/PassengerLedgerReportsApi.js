03abcef381860003731688fcb12aca5624cccfe3
shuvo784 <mrmasudhossain@gmail.com>
2024-09-26 12:25:12 +0600

    send upadte code

--- src/app/main/rams/PassengerLedgerReportManagement/PassengerLedgerReportsApi.js
+++ src/app/main/rams/PassengerLedgerReportManagement/PassengerLedgerReportsApi.js
@@ -3,12 +3,12 @@ import { createSelector } from '@reduxjs/toolkit';
 import { apiService as api } from 'app/store/apiService';
 import jsonToFormData from 'src/app/@helpers/jsonToFormData';
 import {
-	AGENT_FILTER_BY,
-	AGENT_FILTER_WITHOUT_PG,
 	CREATE_DEPARTMENT,
 	DELETE_DEPARTMENT,
 	DELETE_DEPARTMENT_MULTIPLE,
 	GET_DEPARTMENT_BY_ID,
+	GET_PASSENGER_LEDGER_REPORT,
+	GET_PASSENGER_LEDGER_REPORT_WITHOUT_PG,
 	UPDATE_DEPARTMENT
 } from 'src/app/constant/constants';
 import { selectSearchText } from './store/searchTextSlice';
@@ -22,14 +22,14 @@ const PassengerLedgerReportApi = api
 		endpoints: (build) => ({
 			getPassengerLedgerReports: build.query({
 				query: (filterData) => ({
-					url: AGENT_FILTER_BY,
+					url: GET_PASSENGER_LEDGER_REPORT,
 					params: filterData
 				}),
 				providesTags: ['passengerLedgerReports']
 			}),
 			getPassengerLedgerAllReports: build.query({
 				query: (filterData) => ({
-					url: AGENT_FILTER_WITHOUT_PG,
+					url: GET_PASSENGER_LEDGER_REPORT_WITHOUT_PG,
 					params: filterData
 				}),
 				providesTags: ['passengerLedgerReports']
