e57753bd0cd9c00350ec7ea9042dce9a14c46667
shuvo784 <mrmasudhossain@gmail.com>
2024-09-30 13:10:12 +0600

    send update code

--- src/app/@components/ReportComponents/SinglePage.jsx
+++ src/app/@components/ReportComponents/SinglePage.jsx
@@ -44,7 +44,7 @@ function SinglePage({
 		<div
 			className={`${classes.pageContainer} printPageContainer px-60 `}
 			onMouseOver={() => {
-				inSiglePageMode || setPage(data?.page);
+				inSiglePageMode 
 			}}
 		>
 			<br />
