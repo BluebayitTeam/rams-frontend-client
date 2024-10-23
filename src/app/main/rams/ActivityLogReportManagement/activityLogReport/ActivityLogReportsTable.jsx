438ebf2055f05cfe4462ceaeaa44f5576b8d5f73
shuvo784 <mrmasudhossain@gmail.com>
2024-10-23 15:33:32 +0600

    send a update code

--- src/app/main/rams/ActivityLogReportManagement/activityLogReport/ActivityLogReportsTable.jsx
+++ src/app/main/rams/ActivityLogReportManagement/activityLogReport/ActivityLogReportsTable.jsx
@@ -75,7 +75,7 @@ function ActivityLogReportsTable(props) {
     initialTableColumnsState
   );
   const [page, setPage] = useState(1);
-  const [size, setSize] = useState(10);
+  const [size, setSize] = useState(25);
   const [totalPages, setTotalPages] = useState(0);
   const [totalElements, setTotalElements] = useState(0);
   const [inShowAllMode, setInShowAllMode] = useState(false);
