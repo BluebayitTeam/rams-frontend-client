a71c738c86a92cc355dc38c5caad074d980de606
shuvo784 <mrmasudhossain@gmail.com>
2024-11-03 10:20:17 +0600

    send a update code

--- src/app/main/rams/CompanyOverviewReportManagement/CompanyOverviewReportsApi.js
+++ src/app/main/rams/CompanyOverviewReportManagement/CompanyOverviewReportsApi.js
@@ -8,6 +8,8 @@ import {
   CREATE_DEPARTMENT,
   DELETE_DEPARTMENT,
   DELETE_DEPARTMENT_MULTIPLE,
+  GET_COMPANY_OVERVIEW_REPORT,
+  GET_COMPANY_OVERVIEW_REPORT_WITHOUT_PG,
   GET_DEPARTMENT_BY_ID,
   UPDATE_DEPARTMENT,
 } from 'src/app/constant/constants';
@@ -22,54 +24,17 @@ const CompanyOverviewReportApi = api
     endpoints: (build) => ({
       getCompanyOverviewReports: build.query({
         query: (filterData) => ({
-          url: ACCOUNTSTATEMENT_FILTER_BY,
+          url: GET_COMPANY_OVERVIEW_REPORT,
           params: filterData,
         }),
         providesTags: ['companyOverviewReports'],
       }),
       getCompanyOverviewAllReports: build.query({
         query: (filterData) => ({
-          url: ACCOUNTSTATEMENT_FILTER_WITHOUT_PG,
+          url: GET_COMPANY_OVERVIEW_REPORT_WITHOUT_PG,
           params: filterData,
         }),
         providesTags: ['companyOverviewReports'],
-      }),
-      deleteCompanyOverviewReports: build.mutation({
-        query: (companyOverviewReportIds) => ({
-          url: DELETE_DEPARTMENT_MULTIPLE,
-          method: 'DELETE',
-          data: { ids: companyOverviewReportIds },
-        }),
-        invalidatesTags: ['companyOverviewReports'],
-      }),
-      getCompanyOverviewReport: build.query({
-        query: (companyOverviewReportId) => ({
-          url: `${GET_DEPARTMENT_BY_ID}${companyOverviewReportId}`,
-        }),
-        providesTags: ['companyOverviewReports'],
-      }),
-      createCompanyOverviewReport: build.mutation({
-        query: (newCompanyOverviewReport) => ({
-          url: CREATE_DEPARTMENT,
-          method: 'POST',
-          data: jsonToFormData(newCompanyOverviewReport),
-        }),
-        invalidatesTags: ['companyOverviewReports'],
-      }),
-      updateCompanyOverviewReport: build.mutation({
-        query: (companyOverviewReport) => ({
-          url: `${UPDATE_DEPARTMENT}${companyOverviewReport.id}`,
-          method: 'PUT',
-          data: jsonToFormData(companyOverviewReport),
-        }),
-        invalidatesTags: ['companyOverviewReports'],
-      }),
-      deleteCompanyOverviewReport: build.mutation({
-        query: (companyOverviewReportId) => ({
-          url: `${DELETE_DEPARTMENT}${companyOverviewReportId}`,
-          method: 'DELETE',
-        }),
-        invalidatesTags: ['companyOverviewReports'],
       }),
     }),
     overrideExisting: false,
@@ -78,11 +43,6 @@ export default CompanyOverviewReportApi;
 export const {
   useGetCompanyOverviewReportsQuery,
   useGetCompanyOverviewAllReportsQuery,
-  useDeleteCompanyOverviewReportsMutation,
-  useGetCompanyOverviewReportQuery,
-  useUpdateCompanyOverviewReportMutation,
-  useDeleteCompanyOverviewReportMutation,
-  useCreateCompanyOverviewReportMutation,
 } = CompanyOverviewReportApi;
 
 export const selectFilteredCompanyOverviewReports = (companyOverviewReports) =>
