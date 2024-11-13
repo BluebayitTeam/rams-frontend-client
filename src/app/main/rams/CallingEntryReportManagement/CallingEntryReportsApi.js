2f493919370cc7f5583a082979ad9b96c260d2d4
shuvo784 <mrmasudhossain@gmail.com>
2024-11-13 15:23:30 +0600

    send a update code CallingEntryReportApi

--- src/app/main/rams/CallingEntryReportManagement/CallingEntryReportsApi.js
+++ src/app/main/rams/CallingEntryReportManagement/CallingEntryReportsApi.js
@@ -3,17 +3,8 @@ import { createSelector } from '@reduxjs/toolkit';
 import { apiService as api } from 'app/store/apiService';
 import jsonToFormData from 'src/app/@helpers/jsonToFormData';
 import {
-  ACCOUNTSTATEMENT_FILTER_BY,
-  ACCOUNTSTATEMENT_FILTER_WITHOUT_PG,
-  CREATE_DEPARTMENT,
-  DELETE_DEPARTMENT,
-  DELETE_DEPARTMENT_MULTIPLE,
-  GET_COMPANY_OVERVIEW_REPORT,
-  GET_COMPANY_OVERVIEW_REPORT_WITHOUT_PG,
-  GET_DEPARTMENT_BY_ID,
-  UPDATE_DEPARTMENT,
-  VISA_ENTRY_FILTER_BY,
-  VISA_ENTRY_FILTER_BY_WP,
+  CALLING_ENTRY_FILTER_BY,
+  CALLING_ENTRY_FILTER_BY_WP,
 } from 'src/app/constant/constants';
 import { selectSearchText } from './store/searchTextSlice';
 
@@ -26,14 +17,14 @@ const CallingEntryReportApi = api
     endpoints: (build) => ({
       getCallingEntryReports: build.query({
         query: (filterData) => ({
-          url: VISA_ENTRY_FILTER_BY,
+          url: CALLING_ENTRY_FILTER_BY,
           params: filterData,
         }),
         providesTags: ['callingEntryReports'],
       }),
       getCallingEntryAllReports: build.query({
         query: (filterData) => ({
-          url: VISA_ENTRY_FILTER_BY_WP,
+          url: CALLING_ENTRY_FILTER_BY_WP,
           params: filterData,
         }),
         providesTags: ['callingEntryReports'],
