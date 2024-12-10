ca8e1f2b1415bf2eee14b7cb62b48c6e45fc98e6
shuvo784 <mrmasudhossain@gmail.com>
2024-12-10 10:04:47 +0600

    send a update code

--- src/app/main/rams/CurrencysManagement/currencys/CurrencysTable.jsx
+++ src/app/main/rams/CurrencysManagement/currencys/CurrencysTable.jsx
@@ -18,264 +18,292 @@ import { Delete, Edit } from '@mui/icons-material';
 import CurrencysTableHead from './CurrencysTableHead';
 import { selectFilteredCurrencys, useGetCurrencysQuery } from '../CurrencysApi';
 import { hasPermission } from 'src/app/constant/permission/permissionList';
+import { makeStyles } from '@mui/styles';
 
-/**
- * The currencys table.
- */
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    position: 'fixed',
+    bottom: 0,
+    backgroundColor: '#fff',
+    padding: '10px 20px',
+    zIndex: 1000,
+    width: '75%',
+  },
+  paginationContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '100%',
+    padding: '0 20px',
+  },
+  pagination: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '10px',
+  },
+}));
 function CurrencysTable(props) {
-	const dispatch = useDispatch();
-	const { navigate, searchKey } = props;
-	const [page, setPage] = useState(0);
-	const [rowsPerPage, setRowsPerPage] = useState(50);
-	const [pageAndSize, setPageAndSize] = useState({ page: 1, size: 25 });
-	const { data, isLoading, refetch } = useGetCurrencysQuery({ ...pageAndSize, searchKey });
-	const totalData = useSelector(selectFilteredCurrencys(data));
-	const currencys = useSelector(selectFilteredCurrencys(data?.currencies));
-	let serialNumber = 1;
+  const dispatch = useDispatch();
+  const { navigate, searchKey } = props;
+   const classes = useStyles();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(50);
+  const [pageAndSize, setPageAndSize] = useState({ page: 1, size: 25 });
+  const { data, isLoading, refetch } = useGetCurrencysQuery({
+    ...pageAndSize,
+    searchKey,
+  });
+  const totalData = useSelector(selectFilteredCurrencys(data));
+  const currencys = useSelector(selectFilteredCurrencys(data?.currencies));
+  let serialNumber = 1;
 
-	useEffect(() => {
-		// Fetch data with specific page and size when component mounts or when page and size change
-		refetch({ page, rowsPerPage });
-	}, [page, rowsPerPage]);
+  useEffect(() => {
+    // Fetch data with specific page and size when component mounts or when page and size change
+    refetch({ page, rowsPerPage });
+  }, [page, rowsPerPage]);
 
-	useEffect(() => {
-		refetch({ searchKey });
-	}, [searchKey]);
-	const [selected, setSelected] = useState([]);
+  useEffect(() => {
+    refetch({ searchKey });
+  }, [searchKey]);
+  const [selected, setSelected] = useState([]);
 
-	const [tableOrder, setTableOrder] = useState({
-		direction: 'asc',
-		id: ''
-	});
+  const [tableOrder, setTableOrder] = useState({
+    direction: 'asc',
+    id: '',
+  });
 
-	function handleRequestSort(event, property) {
-		const newOrder = { id: property, direction: 'desc' };
+  function handleRequestSort(event, property) {
+    const newOrder = { id: property, direction: 'desc' };
 
-		if (tableOrder.id === property && tableOrder.direction === 'desc') {
-			newOrder.direction = 'asc';
-		}
+    if (tableOrder.id === property && tableOrder.direction === 'desc') {
+      newOrder.direction = 'asc';
+    }
 
-		setTableOrder(newOrder);
-	}
+    setTableOrder(newOrder);
+  }
 
-	function handleSelectAllClick(event) {
-		if (event.target.checked) {
-			setSelected(currencys.map((n) => n.id));
-			return;
-		}
+  function handleSelectAllClick(event) {
+    if (event.target.checked) {
+      setSelected(currencys.map((n) => n.id));
+      return;
+    }
 
-		setSelected([]);
-	}
+    setSelected([]);
+  }
 
-	function handleDeselect() {
-		setSelected([]);
-	}
+  function handleDeselect() {
+    setSelected([]);
+  }
 
-	function handleClick(item) {
-		navigate(`/apps/currency/currencys/${item.id}/${item.handle}`);
-	}
+  function handleClick(item) {
+    navigate(`/apps/currency/currencys/${item.id}/${item.handle}`);
+  }
 
-	function handleUpdateCurrency(item, event) {
-		localStorage.removeItem('deleteCurrency');
-		localStorage.setItem('updateCurrency', event);
-		navigate(`/apps/currency/currencys/${item.id}/${item.handle}`);
-	}
+  function handleUpdateCurrency(item, event) {
+    localStorage.removeItem('deleteCurrency');
+    localStorage.setItem('updateCurrency', event);
+    navigate(`/apps/currency/currencys/${item.id}/${item.handle}`);
+  }
 
-	function handleDeleteCurrency(item, event) {
-		localStorage.removeItem('updateCurrency');
-		localStorage.setItem('deleteCurrency', event);
-		navigate(`/apps/currency/currencys/${item.id}/${item.handle}`);
-	}
+  function handleDeleteCurrency(item, event) {
+    localStorage.removeItem('updateCurrency');
+    localStorage.setItem('deleteCurrency', event);
+    navigate(`/apps/currency/currencys/${item.id}/${item.handle}`);
+  }
 
-	function handleCheck(event, id) {
-		const selectedIndex = selected.indexOf(id);
-		let newSelected = [];
+  function handleCheck(event, id) {
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
 
-		if (selectedIndex === -1) {
-			newSelected = newSelected.concat(selected, id);
-		} else if (selectedIndex === 0) {
-			newSelected = newSelected.concat(selected.slice(1));
-		} else if (selectedIndex === selected.length - 1) {
-			newSelected = newSelected.concat(selected.slice(0, -1));
-		} else if (selectedIndex > 0) {
-			newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
-		}
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
 
-		setSelected(newSelected);
-	}
+    setSelected(newSelected);
+  }
 
-	// pagination
-	const handlePagination = (e, handlePage) => {
-		setPageAndSize({ ...pageAndSize, page: handlePage });
-		setPage(handlePage - 1);
-	};
+  // pagination
+  const handlePagination = (e, handlePage) => {
+    setPageAndSize({ ...pageAndSize, page: handlePage });
+    setPage(handlePage - 1);
+  };
 
-	function handleChangePage(event, value) {
-		setPage(value);
-		setPageAndSize({ ...pageAndSize, page: value + 1 });
-	}
+  function handleChangePage(event, value) {
+    setPage(value);
+    setPageAndSize({ ...pageAndSize, page: value + 1 });
+  }
 
-	function handleChangeRowsPerPage(event) {
-		setRowsPerPage(+event.target.value);
-		setPageAndSize({ ...pageAndSize, size: event.target.value });
-	}
+  function handleChangeRowsPerPage(event) {
+    setRowsPerPage(+event.target.value);
+    setPageAndSize({ ...pageAndSize, size: event.target.value });
+  }
 
-	if (isLoading) {
-		return (
-			<div className="flex items-center justify-center h-full">
-				<FuseLoading />
-			</div>
-		);
-	}
+  if (isLoading) {
+    return (
+      <div className='flex items-center justify-center h-full'>
+        <FuseLoading />
+      </div>
+    );
+  }
 
-	if (currencys?.length === 0) {
-		return (
-			<motion.div
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1, transition: { delay: 0.1 } }}
-				className="flex flex-1 items-center justify-center h-full"
-			>
-				<Typography
-					color="text.secondary"
-					variant="h5"
-				>
-					There are no currencys!
-				</Typography>
-			</motion.div>
-		);
-	}
+  if (currencys?.length === 0) {
+    return (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { delay: 0.1 } }}
+        className='flex flex-1 items-center justify-center h-full'>
+        <Typography color='text.secondary' variant='h5'>
+          There are no currencys!
+        </Typography>
+      </motion.div>
+    );
+  }
 
-	return (
-		<div className="w-full flex flex-col min-h-full px-10">
-			<FuseScrollbars className="grow overflow-x-auto">
-				<Table
-					stickyHeader
-					className="min-w-xl"
-					aria-labelledby="tableTitle"
-				>
-					<CurrencysTableHead
-						selectedCurrencyIds={selected}
-						tableOrder={tableOrder}
-						onSelectAllClick={handleSelectAllClick}
-						onRequestSort={handleRequestSort}
-						rowCount={currencys.length}
-						onMenuItemClick={handleDeselect}
-					/>
+  return (
+	   <div className='w-full flex flex-col min-h-full px-10'>
+		        <div className='grow overflow-x-auto overflow-y-auto'>
 
-					<TableBody>
-						{_.orderBy(currencys, [tableOrder.id], [tableOrder.direction]).map((n) => {
-							const isSelected = selected.indexOf(n.id) !== -1;
-							return (
-                <TableRow
-                  className='h-20 cursor-pointer'
-                  hover
-                  role='checkbox'
-                  aria-checked={isSelected}
-                  tabIndex={-1}
-                  key={n.id}
-                  selected={isSelected}>
-                  <TableCell
-                    className='w-40 md:w-64 text-center'
-                    padding='none'
-                    style={{
-                      position: 'sticky',
-                      left: 0,
-                      zIndex: 1,
-                      backgroundColor: '#fff',
-                    }}>
-                    <Checkbox
-                      checked={isSelected}
-                      onClick={(event) => event.stopPropagation()}
-                      onChange={(event) => handleCheck(event, n.id)}
-                    />
-                  </TableCell>
+      <FuseScrollbars className='grow overflow-x-auto'>
+        <Table stickyHeader className='min-w-xl' aria-labelledby='tableTitle'>
+          <CurrencysTableHead
+            selectedCurrencyIds={selected}
+            tableOrder={tableOrder}
+            onSelectAllClick={handleSelectAllClick}
+            onRequestSort={handleRequestSort}
+            rowCount={currencys.length}
+            onMenuItemClick={handleDeselect}
+          />
 
-                  <TableCell
-                    className='w-40 md:w-64'
-                    component='th'
-                    scope='row'
-                    style={{
-                      position: 'sticky',
-                      left: 0,
-                      zIndex: 1,
-                      backgroundColor: '#fff',
-                    }}>
-                    {pageAndSize.page * pageAndSize.size -
-                      pageAndSize.size +
-                      serialNumber++}
-                  </TableCell>
-                  <TableCell className='p-4 md:p-16' component='th' scope='row'>
-                    {n.name}
-                  </TableCell>
-                  <TableCell
-                    className='p-4 md:p-16'
-                    component='th'
-                    scope='row'
-                    align='right'
-                    style={{
-                      position: 'sticky',
-                      right: 0,
-                      zIndex: 1,
-                      backgroundColor: '#fff',
-                    }}>
-                    {hasPermission('CURRENCY_UPDATE') && (
-                      <Edit
-                        onClick={(event) =>
-                          handleUpdateCurrency(n, 'updateCurrency')
-                        }
-                        className='cursor-pointer custom-edit-icon-style'
+          <TableBody>
+            {_.orderBy(currencys, [tableOrder.id], [tableOrder.direction]).map(
+              (n) => {
+                const isSelected = selected.indexOf(n.id) !== -1;
+                return (
+                  <TableRow
+                    className='h-20 cursor-pointer border-t-1  border-gray-200'
+                    hover
+                    role='checkbox'
+                    aria-checked={isSelected}
+                    tabIndex={-1}
+                    key={n.id}
+                    selected={isSelected}>
+                    <TableCell
+                      className='w-40 md:w-64 text-center border-t-1  border-gray-200'
+                      padding='none'
+                      style={{
+                        position: 'sticky',
+                        left: 0,
+                        zIndex: 1,
+                        backgroundColor: '#fff',
+                      }}>
+                      <Checkbox
+                        checked={isSelected}
+                        onClick={(event) => event.stopPropagation()}
+                        onChange={(event) => handleCheck(event, n.id)}
                       />
-                    )}
+                    </TableCell>
 
-                    {hasPermission('CURRENCY_DELETE') && (
-                      <Delete
-                        onClick={(event) =>
-                          handleDeleteCurrency(n, 'deleteCurrency')
-                        }
-                        className='cursor-pointer custom-delete-icon-style'
-                      />
-                    )}
-                  </TableCell>
-                </TableRow>
-              );
-						})}
-					</TableBody>
-				</Table>
-			</FuseScrollbars>
+                    <TableCell
+                      className='w-40 md:w-64 border-t-1  border-gray-200'
+                      component='th'
+                      scope='row'
+                      style={{
+                        position: 'sticky',
+                        left: 0,
+                        zIndex: 1,
+                        backgroundColor: '#fff',
+                      }}>
+                      {pageAndSize.page * pageAndSize.size -
+                        pageAndSize.size +
+                        serialNumber++}
+                    </TableCell>
+                    <TableCell
+                      className='p-4 md:p-16 border-t-1  border-gray-200'
+                      component='th'
+                      scope='row'>
+                      {n.name}
+                    </TableCell>
+                    <TableCell
+                      className='p-4 md:p-16 border-t-1  border-gray-200'
+                      component='th'
+                      scope='row'
+                      align='right'
+                      style={{
+                        position: 'sticky',
+                        right: 0,
+                        zIndex: 1,
+                        backgroundColor: '#fff',
+                      }}>
+                      {hasPermission('CURRENCY_UPDATE') && (
+                        <Edit
+                          onClick={(event) =>
+                            handleUpdateCurrency(n, 'updateCurrency')
+                          }
+                          className='cursor-pointer custom-edit-icon-style'
+                        />
+                      )}
 
-			<div id="pagiContainer">
-				<Pagination
-					// classes={{ ul: 'flex-nowrap' }}
-					count={totalData?.total_pages}
-					page={page + 1}
-					defaultPage={1}
-					color="primary"
-					showFirstButton
-					showLastButton
-					variant="outlined"
-					shape="rounded"
-					onChange={handlePagination}
-				/>
+                      {hasPermission('CURRENCY_DELETE') && (
+                        <Delete
+                          onClick={(event) =>
+                            handleDeleteCurrency(n, 'deleteCurrency')
+                          }
+                          className='cursor-pointer custom-delete-icon-style'
+                        />
+                      )}
+                    </TableCell>
+                  </TableRow>
+                );
+              }
+            )}
+          </TableBody>
+        </Table>
+			  </FuseScrollbars>
+			  </div>
 
-				<TablePagination
-					className="shrink-0 border-t-1"
-					component="div"
-					rowsPerPageOptions={rowsPerPageOptions}
-					count={totalData?.total_pages}
-					rowsPerPage={rowsPerPage}
-					page={page}
-					backIconButtonProps={{
-						'aria-label': 'Previous Page'
-					}}
-					nextIconButtonProps={{
-						'aria-label': 'Next Page'
-					}}
-					onPageChange={handleChangePage}
-					onRowsPerPageChange={handleChangeRowsPerPage}
-				/>
-			</div>
-		</div>
-	);
+       <div className={classes.root} id='pagiContainer'>
+        <Pagination
+          // classes={{ ul: 'flex-nowrap' }}
+          count={totalData?.total_pages}
+          page={page + 1}
+          defaultPage={1}
+          color='primary'
+          showFirstButton
+          showLastButton
+          variant='outlined'
+          shape='rounded'
+          onChange={handlePagination}
+        />
+
+        <TablePagination
+          className='shrink-0 '
+          component='div'
+          rowsPerPageOptions={rowsPerPageOptions}
+          count={totalData?.total_pages}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          backIconButtonProps={{
+            'aria-label': 'Previous Page',
+          }}
+          nextIconButtonProps={{
+            'aria-label': 'Next Page',
+          }}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default withRouter(CurrencysTable);
