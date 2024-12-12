0808885ff577c413bc0ddb3f6e7429fb1bdb6514
shuvo784 <mrmasudhossain@gmail.com>
2024-12-12 13:08:19 +0600

    send a update code

--- src/app/main/rams/PassengerSummaryUpdateColumnManagement/PassengerSummaryUpdatesApi.js
+++ src/app/main/rams/PassengerSummaryUpdateColumnManagement/PassengerSummaryUpdatesApi.js
@@ -7,7 +7,8 @@ import {
 	GET_DEMAND_BY_ID,
 	CREATE_DEMAND,
 	DELETE_DEMAND,
-	UPDATE_DEMAND
+	UPDATE_DEMAND,
+	GET_PASSENGER_UPDATES
 } from 'src/app/constant/constants';
 import jsonToFormData from 'src/app/@helpers/jsonToFormData';
 import { selectSearchText } from './store/searchTextSlice';
@@ -15,65 +16,67 @@ import PassengerSummaryUpdateModel from './passengerSummaryUpdate/models/Passeng
 
 export const addTagTypes = ['passengerSummaryUpdates'];
 const PassengerSummaryUpdateApi = api
-	.enhanceEndpoints({
-		addTagTypes
-	})
-	.injectEndpoints({
-		endpoints: (build) => ({
-			getPassengerSummaryUpdates: build.query({
-				query: ({ page, size, searchKey }) => ({
-					url: GET_DEMANDS,
-					params: { page, size, searchKey }
-				}),
-				providesTags: ['passengerSummaryUpdates']
-			}),
-			getMultiplePassengers: build.query({
-				query: ({ page, size, searchKey }) => ({
-					url: GET_DEMANDS,
-					params: { page, size, searchKey }
-				}),
-				providesTags: ['passengerSummaryUpdates']
-			}),
-			deletePassengerSummaryUpdates: build.mutation({
-				query: (passengerSummaryUpdateIds) => ({
-					url: ALL_USERS,
-					method: 'DELETE',
-					data: passengerSummaryUpdateIds
-				}),
-				invalidatesTags: ['passengerSummaryUpdates']
-			}),
-			getPassengerSummaryUpdate: build.query({
-				query: (passengerSummaryUpdateId) => ({
-					url: `${GET_DEMAND_BY_ID}${passengerSummaryUpdateId}`
-				}),
-				providesTags: ['passengerSummaryUpdates']
-			}),
-			createPassengerSummaryUpdate: build.mutation({
-				query: (newPassengerSummaryUpdate) => ({
-					url: CREATE_DEMAND,
-					method: 'POST',
-					data: jsonToFormData(PassengerSummaryUpdateModel(newPassengerSummaryUpdate))
-				}),
-				invalidatesTags: ['passengerSummaryUpdates']
-			}),
-			updatePassengerSummaryUpdate: build.mutation({
-				query: (passengerSummaryUpdate) => ({
-					url: `${UPDATE_DEMAND}${passengerSummaryUpdate.id}`,
-					method: 'PUT',
-					data: jsonToFormData(passengerSummaryUpdate)
-				}),
-				invalidatesTags: ['passengerSummaryUpdates']
-			}),
-			deletePassengerSummaryUpdate: build.mutation({
-				query: (passengerSummaryUpdateId) => ({
-					url: `${DELETE_DEMAND}${passengerSummaryUpdateId}`,
-					method: 'DELETE'
-				}),
-				invalidatesTags: ['passengerSummaryUpdates']
-			})
-		}),
-		overrideExisting: false
-	});
+  .enhanceEndpoints({
+    addTagTypes,
+  })
+  .injectEndpoints({
+    endpoints: (build) => ({
+      getPassengerSummaryUpdates: build.query({
+        query: ({ page, size}) => ({
+          url: GET_PASSENGER_UPDATES,
+          params: { page, size},
+        }),
+        providesTags: ['passengerSummaryUpdates'],
+      }),
+      getMultiplePassengers: build.query({
+        query: ({ page, size, searchKey }) => ({
+          url: GET_DEMANDS,
+          params: { page, size, searchKey },
+        }),
+        providesTags: ['passengerSummaryUpdates'],
+      }),
+      deletePassengerSummaryUpdates: build.mutation({
+        query: (passengerSummaryUpdateIds) => ({
+          url: ALL_USERS,
+          method: 'DELETE',
+          data: passengerSummaryUpdateIds,
+        }),
+        invalidatesTags: ['passengerSummaryUpdates'],
+      }),
+      getPassengerSummaryUpdate: build.query({
+        query: (passengerSummaryUpdateId) => ({
+          url: `${GET_DEMAND_BY_ID}${passengerSummaryUpdateId}`,
+        }),
+        providesTags: ['passengerSummaryUpdates'],
+      }),
+      createPassengerSummaryUpdate: build.mutation({
+        query: (newPassengerSummaryUpdate) => ({
+          url: CREATE_DEMAND,
+          method: 'POST',
+          data: jsonToFormData(
+            PassengerSummaryUpdateModel(newPassengerSummaryUpdate)
+          ),
+        }),
+        invalidatesTags: ['passengerSummaryUpdates'],
+      }),
+      updatePassengerSummaryUpdate: build.mutation({
+        query: (passengerSummaryUpdate) => ({
+          url: `${UPDATE_DEMAND}${passengerSummaryUpdate.id}`,
+          method: 'PUT',
+          data: jsonToFormData(passengerSummaryUpdate),
+        }),
+        invalidatesTags: ['passengerSummaryUpdates'],
+      }),
+      deletePassengerSummaryUpdate: build.mutation({
+        query: (passengerSummaryUpdateId) => ({
+          url: `${DELETE_DEMAND}${passengerSummaryUpdateId}`,
+          method: 'DELETE',
+        }),
+        invalidatesTags: ['passengerSummaryUpdates'],
+      }),
+    }),
+    overrideExisting: false,
+  });
 export default PassengerSummaryUpdateApi;
 export const {
 	useGetPassengerSummaryUpdatesQuery,
