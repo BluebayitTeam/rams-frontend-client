c8e2379f429a49b8b0641f7b5c976157af032ddb
tonmoy-dev <rxdevs100@gmail.com>
2025-01-19 15:54:31 +0600

    fix: add header updates in payroll settings

--- src/app/main/rams/UnitsManagement/units/UnitsHeader.jsx
+++ src/app/main/rams/UnitsManagement/units/UnitsHeader.jsx
@@ -1,16 +1,17 @@
+import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
+import NavLinkAdapter from '@fuse/core/NavLinkAdapter';
+import { Icon } from '@mui/material';
 import Button from '@mui/material/Button';
 import Input from '@mui/material/Input';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import { motion } from 'framer-motion';
 import { useAppDispatch } from 'app/store/store';
-import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
+import { motion } from 'framer-motion';
 import { useEffect } from 'react';
-import NavLinkAdapter from '@fuse/core/NavLinkAdapter';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
-import { resetSearchText, selectSearchText } from '../store/searchTextSlice';
 import { hasPermission } from 'src/app/constant/permission/permissionList';
+import { resetSearchText, selectSearchText } from '../store/searchTextSlice';
 
 /**
  * The units header.
@@ -27,13 +28,25 @@ function UnitsHeader(props) {
   }, []);
   return (
     <div className='flex flex-col sm:flex-row space-y-12 sm:space-y-0 flex-1 w-full justify-between py-32 px-24 md:px-32'>
-      <motion.span
-        initial={{ x: -20 }}
-        animate={{ x: 0, transition: { delay: 0.2 } }}>
-        <Typography className='text-24 md:text-32 font-extrabold tracking-tight'>
+      <div className="flex items-center">
+        <Icon
+          component={motion.span}
+          initial={{ scale: 0 }}
+          animate={{ scale: 1, transition: { delay: 0.2 } }}
+          className="text-24 md:text-32"
+        >
+          person
+        </Icon>
+        <Typography
+          component={motion.span}
+          initial={{ x: -10 }}
+          animate={{ x: 0, transition: { delay: 0.2 } }}
+          delay={300}
+          className="hidden sm:flex text-16 md:text-24 mx-12 font-semibold"
+        >
           Units
         </Typography>
-      </motion.span>
+      </div>
 
       <div className='flex w-full sm:w-auto flex-1 items-center justify-end space-x-8'>
         <Paper
