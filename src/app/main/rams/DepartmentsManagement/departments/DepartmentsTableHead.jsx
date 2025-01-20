759be24ee0b4edbd55900c21fc531252c18791f3
shuvo784 <mrmasudhossain@gmail.com>
2025-01-20 13:13:58 +0600

    feat: refactor DepartmentsTableHead component for improved readability and structure

--- src/app/main/rams/DepartmentsManagement/departments/DepartmentsTableHead.jsx
+++ src/app/main/rams/DepartmentsManagement/departments/DepartmentsTableHead.jsx
@@ -13,109 +13,107 @@ import { useDeleteDepartmentsMutation } from '../DepartmentsApi';
  */
 
 const rows = [
-	{
-		id: 'SL',
-		align: 'left',
-		disablePadding: true,
-		label: 'SL',
-		sort: true
-	},
-	{
-		id: 'name',
-		align: 'left',
-		disablePadding: false,
-		label: 'Name',
-		sort: true
-	},
-	{
-		id: 'action',
-		align: 'right',
-		disablePadding: false,
-		label: 'Action',
-		sort: true
-	}
+  {
+    id: 'SL',
+    align: 'left',
+    disablePadding: true,
+    label: 'SL',
+    sort: true,
+  },
+  {
+    id: 'name',
+    align: 'left',
+    disablePadding: false,
+    label: 'Name',
+    sort: true,
+  },
+  {
+    id: 'action',
+    align: 'right',
+    disablePadding: false,
+    label: 'Action',
+    sort: true,
+  },
 ];
 
 function DepartmentsTableHead(props) {
-	const { selectedDepartmentIds, tableOrder, onSelectAllClick, onRequestSort, rowCount, onMenuItemClick } = props;
+  const {
+    selectedDepartmentIds,
+    tableOrder,
+    onSelectAllClick,
+    onRequestSort,
+    rowCount,
+    onMenuItemClick,
+  } = props;
 
-	console.log('onMenuItemClick', onMenuItemClick);
+  console.log('onMenuItemClick', onMenuItemClick);
 
-	console.log('selectedDepartmentIds', selectedDepartmentIds);
+  console.log('selectedDepartmentIds', selectedDepartmentIds);
 
-	const [removeDepartments] = useDeleteDepartmentsMutation();
-	const numSelected = selectedDepartmentIds.length;
-	const [selectedDepartmentsMenu, setSelectedDepartmentsMenu] = useState(null);
-	const createSortHandler = (event, property) => {
-		onRequestSort(event, property);
-	};
+  const [removeDepartments] = useDeleteDepartmentsMutation();
+  const numSelected = selectedDepartmentIds.length;
+  const [selectedDepartmentsMenu, setSelectedDepartmentsMenu] = useState(null);
+  const createSortHandler = (event, property) => {
+    onRequestSort(event, property);
+  };
 
-	function openSelectedDepartmentsMenu(event) {
-		setSelectedDepartmentsMenu(event.currentTarget);
-	}
+  function openSelectedDepartmentsMenu(event) {
+    setSelectedDepartmentsMenu(event.currentTarget);
+  }
 
-	function closeSelectedDepartmentsMenu() {
-		setSelectedDepartmentsMenu(null);
-	}
+  function closeSelectedDepartmentsMenu() {
+    setSelectedDepartmentsMenu(null);
+  }
 
-	function handleDeleteMultipleItem() {
-		removeDepartments(selectedDepartmentIds).then((data) => {
-			Swal.fire({
-				position: 'top-center',
-				icon: 'success',
-				title: 'Deleted Successfully',
-				showConfirmButton: false,
-				timer: 2000
-			});
-		});
-	}
+  function handleDeleteMultipleItem() {
+    removeDepartments(selectedDepartmentIds).then((data) => {
+      Swal.fire({
+        position: 'top-center',
+        icon: 'success',
+        title: 'Deleted Successfully',
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    });
+  }
 
-	return (
-		<TableHead>
-			<TableRow className="h-48 sm:h-64">
-				{rows.map((row, index, array) => {
-					return (
-						<TableCell
-							sx={{
-								backgroundColor: (theme) =>
-									theme.palette.mode === 'light'
-										? lighten(theme.palette.background.default, 0.4)
-										: lighten(theme.palette.background.default, 0.02)
-							}}
-							className="p-4 md:p-16 whitespace-nowrap"
-							key={row.id}
-							align={row.align}
-							padding={row.disablePadding ? 'none' : 'normal'}
-							style={{
-								position: index === 0 || index === array.length - 1 ? 'sticky' : 'inherit',
-								left: index === 0 ? 0 : undefined,
-								right: index === array.length - 1 ? 0 : undefined,
-								zIndex: index === 0 || index === array.length - 1 ? 1 : 'auto'
-							}}
-							sortDirection={tableOrder.id === row.id ? tableOrder.direction : false}
-						>
-							{row.sort && (
-								<Tooltip
-									title="Sort"
-									placement={row.align === 'right' ? 'bottom-end' : 'bottom-start'}
-									enterDelay={300}
-								>
-									<TableSortLabel
-										active={tableOrder.id === row.id}
-										direction={tableOrder.direction}
-										onClick={(ev) => createSortHandler(ev, row.id)}
-										className="font-semibold"
-									>
-										{row.label}
-									</TableSortLabel>
-								</Tooltip>
-							)}
-						</TableCell>
-					);
-				})}
-			</TableRow>
-		</TableHead>
-	);
+  return (
+    <TableHead>
+      <TableRow className='h-48 sm:h-64'>
+        {rows.map((row, index, array) => (
+          <TableCell
+            sx={{
+              backgroundColor: (theme) =>
+                theme.palette.mode === 'light'
+                  ? lighten(theme.palette.background.default, 0.4)
+                  : lighten(theme.palette.background.default, 0.02),
+            }}
+            key={row.id}
+            align={row.align}
+            padding={row.disablePadding ? 'none' : 'normal'}>
+            {row.sort ? (
+              <Tooltip
+                title='Sort'
+                placement={
+                  row.align === 'right' ? 'bottom-end' : 'bottom-start'
+                }
+                enterDelay={300}>
+                <TableSortLabel
+                  active={tableOrder.id === row.id}
+                  direction={tableOrder.direction}
+                  onClick={(ev) => createSortHandler(ev, row.id)}
+                  className='font-semibold'>
+                  {row.label}
+                </TableSortLabel>
+              </Tooltip>
+            ) : (
+              row.label
+            )}
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
 }
 
 export default DepartmentsTableHead;
