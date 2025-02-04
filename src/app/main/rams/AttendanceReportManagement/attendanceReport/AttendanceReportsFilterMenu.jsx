6097e23bd75408c1a43884cc91c9a214d12eee82
shuvo784 <mrmasudhossain@gmail.com>
2025-02-04 15:36:57 +0600

    feat: implement Attendance Reports management with filtering and routing

---
+++ src/app/main/rams/AttendanceReportManagement/attendanceReport/AttendanceReportsFilterMenu.jsx
@@ -0,0 +1,131 @@
+import { useTheme } from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
+import {
+  getDepartments,
+  getEmployees,
+  getLedgers,
+  getSubLedgers,
+} from 'app/store/dataSlice';
+import { useEffect, useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import Keyword from 'src/app/@components/ReportComponents/Keyword';
+import ReportDatePicker from 'src/app/@components/ReportComponents/ReportDatePicker';
+import ReportSelect from 'src/app/@components/ReportComponents/ReportSelect';
+import { bankAndCash } from 'src/app/@data/data';
+import { getReportFilterMakeStyles } from '../../ReportUtilities/reportMakeStyls';
+import ReportSelectFirstLastName from 'src/app/@components/ReportComponents/ReportSelectFirstLastName';
+
+const useStyles = makeStyles((theme) => ({
+  ...getReportFilterMakeStyles(theme),
+}));
+
+function AttendanceReportsFilterMenu({
+  inShowAllMode,
+  handleGetAttendanceReportss,
+  handleGetAllAttendanceReportss,
+}) {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const methods = useFormContext();
+  const { getValues } = methods;
+
+  const theme = useTheme();
+  const { employees, departments } = useSelector((state) => state.data);
+  const values = getValues();
+  const [_reRender, setReRender] = useState(0);
+  const commonFieldProps = {
+    setReRender,
+    onEnter: () =>
+      inShowAllMode
+        ? handleGetAllAttendanceReportss()
+        : handleGetAttendanceReportss(),
+  };
+  const commonKewordProps = {
+    setReRender,
+    onClick: () =>
+      inShowAllMode
+        ? handleGetAllAttendanceReportss()
+        : handleGetAttendanceReportss(),
+  };
+
+  useEffect(() => {
+    dispatch(getEmployees());
+    dispatch(getDepartments());
+  }, []);
+
+  return (
+    <div className={classes.filterMenuContainer}>
+      <div className='allFieldContainer borderTop mt-4'>
+        {/* date from */}
+        <ReportDatePicker
+          {...commonFieldProps}
+          name='date_from'
+          label='Date From'
+          maxDate={values.date_to || new Date()}
+        />
+
+        {/* date to */}
+        <ReportDatePicker
+          {...commonFieldProps}
+          name='date_to'
+          label='Date To'
+          minDate={values.date_from}
+          maxDate={new Date()}
+        />
+
+        {/* ledger */}
+        <ReportSelectFirstLastName
+          {...commonFieldProps}
+          name='employee'
+          options={employees}
+          icon='import_contacts'
+          width='70px'
+        />
+
+        {/* sub_ledger */}
+        <ReportSelect
+          {...commonFieldProps}
+          name='department'
+          options={departments}
+          icon='import_contacts'
+          width='88px'
+        />
+      </div>
+
+      {/* keywords */}
+      <div className='allKeyWrdContainer'>
+        <Keyword
+          {...commonKewordProps}
+          type='date'
+          name='date_from'
+          label='Date From'
+        />
+
+        <Keyword
+          {...commonKewordProps}
+          type='date'
+          name='date_to'
+          label='Date To'
+        />
+
+        <Keyword
+          {...commonKewordProps}
+          type='select'
+          name='employee'
+          icon='import_contacts'
+        />
+
+        <Keyword
+          {...commonKewordProps}
+          type='select'
+          name='department'
+          icon='import_contacts'
+        />
+      </div>
+    </div>
+  );
+}
+
+export default AttendanceReportsFilterMenu;
---
+++ src/app/main/rams/AttendanceReportManagement/attendanceReport/AttendanceReportsReport.jsx
@@ -0,0 +1,40 @@
+import FusePageCarded from '@fuse/core/FusePageCarded';
+import useThemeMediaQuery from '@fuse/hooks/useThemeMediaQuery';
+import { z } from 'zod';
+import AttendanceReportsReportsTable from './AttendanceReportsReportsTable';
+/**
+ * Form Validation Schema
+ */
+const schema = z.object({
+  first_name: z
+    .string()
+    .nonempty('You must enter a attendancereportsReport name')
+    .min(5, 'The attendancereportsReport name must be at least 5 characters'),
+});
+
+function AttendanceReportsReport() {
+  const isMobile = useThemeMediaQuery((theme) => theme.breakpoints.down('lg'));
+
+  return (
+    <FusePageCarded
+      headerBgHeight='102px'
+      className='bg-grey-300'
+      classes={{
+        content: 'bg-grey-300',
+        contentCard: 'overflow-hidden',
+        header: 'min-h-52 h-52',
+      }}
+      header={
+        <div className='flex'>
+          <h1 className='hidden sm:flex text-16 md:text-24 mt-5 mx-12 font-semibold'>
+            Salary Slips Report
+          </h1>
+        </div>
+      }
+      content={<AttendanceReportsReportsTable />}
+      innerScroll
+    />
+  );
+}
+
+export default AttendanceReportsReport;
