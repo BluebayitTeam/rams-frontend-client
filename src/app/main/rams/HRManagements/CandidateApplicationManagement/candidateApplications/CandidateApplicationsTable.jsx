cc628041d419d9a954d35734db362e08933fe36a
shuvo784 <mrmasudhossain@gmail.com>
2025-02-09 17:38:15 +0600

    feat: adjust padding and height in table components for improved layout consistency

--- src/app/main/rams/HRManagements/CandidateApplicationManagement/candidateApplications/CandidateApplicationsTable.jsx
+++ src/app/main/rams/HRManagements/CandidateApplicationManagement/candidateApplications/CandidateApplicationsTable.jsx
@@ -46,9 +46,9 @@ const useStyles = makeStyles(() => ({
     justifyContent: 'space-between',
     alignItems: 'center',
     position: 'fixed',
-    bottom: 15,
+    bottom: 12,
+    padding: '0px 20px 10px 20px',
     backgroundColor: '#fff',
-    padding: '10px 20px',
     zIndex: 1000,
     borderTop: '1px solid #ddd',
     width: 'calc(100% - 350px)',
@@ -235,7 +235,7 @@ function CandidateApplicationsTable(props) {
       <FuseScrollbars className='grow overflow-x-auto'>
         <TableContainer
           sx={{
-            height: 'calc(100vh - 250px)',
+            height: 'calc(100vh - 248px)',
             overflowY: 'auto',
           }}>
           <Table stickyHeader className='min-w-xl' aria-labelledby='tableTitle'>
--- src/app/main/rams/HRManagements/LeaveApplicationManagement/LeaveApplications/LeaveApplicationsTable.jsx
+++ src/app/main/rams/HRManagements/LeaveApplicationManagement/LeaveApplications/LeaveApplicationsTable.jsx
@@ -34,9 +34,9 @@ const useStyles = makeStyles(() => ({
     justifyContent: 'space-between',
     alignItems: 'center',
     position: 'fixed',
-    bottom: 15,
+    bottom: 12,
+    padding: '0px 20px 10px 20px',
     backgroundColor: '#fff',
-    padding: '10px 20px',
     zIndex: 1000,
     borderTop: '1px solid #ddd',
     width: 'calc(100% - 350px)',
@@ -193,7 +193,7 @@ function LeaveApplicationsTable(props) {
       <FuseScrollbars className='grow overflow-x-auto'>
         <TableContainer
           sx={{
-            height: 'calc(100vh - 250px)',
+            height: 'calc(100vh - 248px)',
             overflowY: 'auto',
           }}>
           <Table stickyHeader className='min-w-xl' aria-labelledby='tableTitle'>
