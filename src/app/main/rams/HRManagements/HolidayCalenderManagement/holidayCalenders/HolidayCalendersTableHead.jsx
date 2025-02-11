2c020dd4db0a1894b9959c24dba14246ff607ba0
shuvo784 <mrmasudhossain@gmail.com>
2025-02-11 15:34:26 +0600

    fix: improve layout and accessibility in HolidayCalendersHeader and enhance sticky header behavior in HolidayCalendersTableHead

--- src/app/main/rams/HRManagements/HolidayCalenderManagement/holidayCalenders/HolidayCalendersTableHead.jsx
+++ src/app/main/rams/HRManagements/HolidayCalenderManagement/holidayCalenders/HolidayCalendersTableHead.jsx
@@ -95,17 +95,41 @@ function HolidayCalendersTableHead(props) {
   }
 
   return (
-    <TableHead>
+    <TableHead
+      sx={{
+        position: 'sticky',
+        top: 0, // Fix the header at the top
+        zIndex: 10, // Ensure it stays on top
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'light'
+            ? lighten(theme.palette.background.default, 0.4)
+            : lighten(theme.palette.background.default, 0.02),
+      }}>
       <TableRow className='h-48 sm:h-64'>
         {rows.map((row, index, array) => {
           return (
             <TableCell
               sx={{
+                position:
+                  index === 0 || index === array.length - 1
+                    ? 'sticky'
+                    : 'inherit',
+                left: index === 0 ? 0 : undefined,
+                right: index === array.length - 1 ? 0 : undefined,
+                zIndex: index === 0 || index === array.length - 1 ? 2 : 'auto',
                 backgroundColor: (theme) =>
                   theme.palette.mode === 'light'
                     ? lighten(theme.palette.background.default, 0.4)
                     : lighten(theme.palette.background.default, 0.02),
+                fontWeight: 'bold',
               }}
+              className='p-4 md:p-16 whitespace-nowrap'
+              key={row.id}
+              align={row.align}
+              padding={row.disablePadding ? 'none' : 'normal'}
+              sortDirection={
+                tableOrder.id === row.id ? tableOrder.direction : false
+              }
               className='p-4 md:p-16 whitespace-nowrap'
               key={row.id}
               align={row.align}
