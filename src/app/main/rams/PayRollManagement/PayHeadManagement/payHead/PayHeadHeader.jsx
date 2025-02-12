1f259121c6b88078e9b5436328d31ca9a48bf709
shuvo784 <mrmasudhossain@gmail.com>
2025-02-12 10:06:53 +0600

    fix: update button styles and clean up conditional rendering in PayHeadHeader component

--- src/app/main/rams/PayRollManagement/PayHeadManagement/payHead/PayHeadHeader.jsx
+++ src/app/main/rams/PayRollManagement/PayHeadManagement/payHead/PayHeadHeader.jsx
@@ -70,7 +70,7 @@ function PayHeadHeader() {
 
   return (
     <div className='flex flex-col sm:flex-row flex-1 w-full items-center justify-between space-y-8 sm:space-y-0 py-24 sm:py-32 px-24 md:px-32'>
-      <div className='flex flex-col items-start space-y-8 sm:space-y-0 w-full sm:max-w-full min-w-0'>
+      <div className='flex flex-col items-start space-y-8 w-2/3  sm:space-y-0 sm:max-w-full min-w-0'>
         <motion.div
           initial={{ x: 20, opacity: 0 }}
           animate={{ x: 0, opacity: 1, transition: { delay: 0.3 } }}>
@@ -99,50 +99,52 @@ function PayHeadHeader() {
             Do you want to remove this payHead?
           </Typography>
         )}
-        {handleDelete === 'deletePayHead' &&
-          payHeadId !== 'new' &&
-          // hasPermission('PAY_HEAD_TYPE_DELETE') && (
+        {handleDelete === 'deletePayHead' && payHeadId !== 'new' && (
+          <motion.div
+            className='flex flex-grow-0'
+            initial={{ opacity: 0, x: 20 }}
+            animate={{ opacity: 1, x: 0, transition: { delay: 0.2 } }}>
+            {/* // hasPermission('PAY_HEAD_TYPE_DELETE') && ( */}
+            <Button
+              className='whitespace-nowrap mx-4 text-white bg-red-500 hover:bg-red-800 active:bg-red-700 focus:outline-none focus:ring focus:ring-red-300'
+              variant='contained'
+              color='secondary'
+              onClick={handleRemovePayHead}
+              startIcon={<Icon className='hidden sm:flex'>delete</Icon>}
+              style={{
+                padding: '0 28px',
+              }}>
+              Remove
+            </Button>
+          </motion.div>
+        )}
+        {payHeadId === 'new' && (
+          //  && hasPermission('PAY_HEAD_TYPE_CREATE')
           <Button
             className='whitespace-nowrap mx-4'
             variant='contained'
             color='secondary'
-            onClick={handleRemovePayHead}
-            startIcon={<Icon className='hidden sm:flex'>delete</Icon>}
-            style={{ backgroundColor: '#ea5b78', color: 'white' }}>
-            Remove
+            // disabled={_.isEmpty(dirtyFields) || !isValid}
+            onClick={handleCreatePayHead}>
+            Save
           </Button>
-          // )
-        }
-        {payHeadId === 'new'
-          //  && hasPermission('PAY_HEAD_TYPE_CREATE') 
-          && (
-            <Button
-              className='whitespace-nowrap mx-4'
-              variant='contained'
-              color='secondary'
-              // disabled={_.isEmpty(dirtyFields) || !isValid}
-              onClick={handleCreatePayHead}>
-              Save
-            </Button>
-          )}
+        )}
         {handleDelete !== 'deletePayHead' &&
           handleUpdate === 'updatePayHead' &&
-          payHeadId !== 'new' &&
-          // hasPermission('PAY_HEAD_TYPE_UPDATE') && 
-          (
+          payHeadId !== 'new' && (
+            // hasPermission('PAY_HEAD_TYPE_UPDATE') &&
             <Button
-              className='whitespace-nowrap mx-4'
+              className='whitespace-nowrap mx-4 text-white bg-green-500 hover:bg-green-800 active:bg-green-700 focus:outline-none focus:ring focus:ring-green-300'
               color='secondary'
               variant='contained'
-              style={{ backgroundColor: '#4dc08e', color: 'white' }}
+              // style={{ backgroundColor: '#4dc08e', color: 'white' }}
               onClick={handleUpdatePayHead}>
               Update
             </Button>
           )}
         <Button
-          className='whitespace-nowrap mx-4'
+          className='whitespace-nowrap mx-4 text-white bg-orange-500 hover:bg-orange-800 active:bg-orange-700 focus:outline-none focus:ring focus:ring-orange-300'
           variant='contained'
-          style={{ backgroundColor: '#FFAA4C', color: 'white' }}
           onClick={handleCancel}>
           Cancel
         </Button>
