ad1025a360485ecf2f00ef1e296dd00b05ee7889
shuvo784 <mrmasudhossain@gmail.com>
2025-02-24 12:25:55 +0600

    fix: update PassengerHeader layout and button styles for improved responsiveness and usability

--- src/app/main/rams/PassengersManagement/Passenger/PassengerHeader.jsx
+++ src/app/main/rams/PassengersManagement/Passenger/PassengerHeader.jsx
@@ -97,7 +97,7 @@ function PassengerHeader() {
 
   return (
     <div className='flex flex-col sm:flex-row flex-1 w-full items-center justify-between space-y-8 sm:space-y-0 py-24 sm:py-32 px-24 md:px-32'>
-      <div className='flex flex-col items-start space-y-8 sm:space-y-0 w-full sm:max-w-full min-w-0'>
+      <div className='flex flex-col items-start space-y-8 sm:space-y-0 w-2/3 sm:max-w-full min-w-0'>
         <motion.div
           initial={{ x: 20, opacity: 0 }}
           animate={{ x: 0, opacity: 1, transition: { delay: 0.3 } }}>
@@ -178,6 +178,7 @@ function PassengerHeader() {
             <Button
               className='whitespace-nowrap mx-2 text-white bg-red-400 hover:bg-red-800 active:bg-red-700 focus:outline-none focus:ring focus:ring-[#ea5b78]-300'
               variant='contained'
+              style={{ padding: '0 28px' }}
               onClick={handleRemovePassenger}
               startIcon={<Icon className='hidden sm:flex'>delete</Icon>}>
               Remove
@@ -198,7 +199,7 @@ function PassengerHeader() {
           passengerId !== 'new' &&
           hasPermission('PASSENGER_UPDATE') && (
             <Button
-              className='whitespace-nowrap mx-2 text-white bg-[#4dc08e]-500 hover:bg-[#4dc08e]-800 active:bg-[#4dc08e]-700 focus:outline-none focus:ring focus:ring-[#4dc08e]-300'
+              className='whitespace-nowrap mx-4 text-white bg-green-500 hover:bg-green-800 active:bg-green-700 focus:outline-none focus:ring focus:ring-green-300'
               color='secondary'
               variant='contained'
               onClick={handleUpdatePassenger}>
