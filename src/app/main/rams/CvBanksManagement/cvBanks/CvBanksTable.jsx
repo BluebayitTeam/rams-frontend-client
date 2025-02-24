8d4b6340d6b502a94ba758fd1a8177208636bd08
shuvo784 <mrmasudhossain@gmail.com>
2025-02-24 15:38:20 +0600

    fix: enhance ComplainsTable and CvMalesTableHead for improved layout and sticky header functionality

--- src/app/main/rams/CvBanksManagement/cvBanks/CvBanksTable.jsx
+++ src/app/main/rams/CvBanksManagement/cvBanks/CvBanksTable.jsx
@@ -12,7 +12,7 @@ import FuseLoading from '@fuse/core/FuseLoading';
 import { useSelector } from 'react-redux';
 import PrintIcon from '@mui/icons-material/Print';
 
-import { Pagination, TableCell } from '@mui/material';
+import { Pagination, TableCell, TableContainer } from '@mui/material';
 import { Delete, Edit, PictureAsPdf } from '@mui/icons-material';
 import { rowsPerPageOptions } from 'src/app/@data/data';
 import { useForm } from 'react-hook-form';
@@ -24,9 +24,36 @@ import PrintCvBank from '@fuse/utils/Print/PrintCvBank';
 import CvBanksTableHead from './CvBanksTableHead';
 import { selectFilteredCvBanks, useGetCvBanksQuery } from '../CvBanksApi';
 import { hasPermission } from 'src/app/constant/permission/permissionList';
-
+import { makeStyles } from '@mui/styles';
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    position: 'fixed',
+    bottom: 12,
+    padding: '0px 20px 10px 20px',
+    backgroundColor: '#fff',
+    zIndex: 1000,
+    borderTop: '1px solid #ddd',
+    width: 'calc(100% - 350px)',
+  },
+  paginationContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '100%',
+    padding: '0 20px',
+  },
+  pagination: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '10px',
+  },
+}));
 function CvBanksTable(props) {
   const { navigate, searchKey } = props;
+  const classes = useStyles();
+
   const { _setValue } = useForm({
     mode: 'onChange',
     resolver: zodResolver(),
@@ -201,166 +228,180 @@ function CvBanksTable(props) {
     <div className='w-full flex flex-col min-h-full px-10 '>
       <PrintCvBank ref={printCvBankRef} title='printCvBank' type='CV' />
       <div className='grow overflow-x-auto overflow-y-auto'>
-        <Table stickyHeader className='min-w-xl ' aria-labelledby='tableTitle'>
-          <CvBanksTableHead
-            selectedCvBankIds={selected}
-            tableOrder={tableOrder}
-            onSelectAllClick={handleSelectAllClick}
-            onRequestSort={handleRequestSort}
-            rowCount={bankCvs?.length}
-            onMenuItemClick={handleDeselect}
-            rows={rows}
-          />
+        <TableContainer
+          sx={{
+            height: 'calc(100vh - 248px)',
+            overflowY: 'auto',
+          }}>
+          <Table
+            stickyHeader
+            className='min-w-xl '
+            aria-labelledby='tableTitle'>
+            <CvBanksTableHead
+              selectedCvBankIds={selected}
+              tableOrder={tableOrder}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={bankCvs?.length}
+              onMenuItemClick={handleDeselect}
+              rows={rows}
+            />
 
-          <TableBody>
-            {_.orderBy(bankCvs, [tableOrder.id], [tableOrder.direction])
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((n, index) => {
-                const isSelected = selected.indexOf(n.id) !== -1;
-                return (
-                  <TableRow
-                    className='h-20 cursor-pointer border-t-1  border-gray-200'
-                    hover
-                    role='checkbox'
-                    aria-checked={isSelected}
-                    tabIndex={-1}
-                    key={n.id}
-                    selected={isSelected}>
-                    <TableCell
-                      className='w-40 md:w-64 border-t-1  border-gray-200'
-                      component='th'
-                      scope='row'
-                      style={{
-                        position: 'sticky',
-                        left: 0,
-                        zIndex: 1,
-                        backgroundColor: '#fff',
-                      }}>
-                      {page * rowsPerPage + index + 1}
-                    </TableCell>
+            <TableBody>
+              {_.orderBy(bankCvs, [tableOrder.id], [tableOrder.direction])
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((n, index) => {
+                  const isSelected = selected.indexOf(n.id) !== -1;
+                  return (
+                    <TableRow
+                      className='h-20 cursor-pointer border-t-1  border-gray-200'
+                      hover
+                      role='checkbox'
+                      aria-checked={isSelected}
+                      tabIndex={-1}
+                      key={n.id}
+                      selected={isSelected}>
+                      <TableCell
+                        className='w-40 md:w-64 border-t-1  border-gray-200'
+                        component='th'
+                        scope='row'
+                        style={{
+                          position: 'sticky',
+                          left: 0,
+                          zIndex: 1,
+                          backgroundColor: '#fff',
+                        }}>
+                        {page * rowsPerPage + index + 1}
+                      </TableCell>
 
-                    {Object.entries(n).map(
-                      ([key, value]) =>
-                        key !== 'id' &&
-                        key !== 'random_number' && (
-                          <TableCell
-                            className='p-4 md:p-16 border-t-1 border-gray-200'
-                            component='th'
-                            scope='row'
-                            key={key}>
-                            {key === 'file' ? (
-                              n[key]?.split('.').pop()?.toLowerCase() ===
-                              'pdf' ? (
-                                <PictureAsPdf
-                                  style={{
-                                    color: 'red',
-                                    cursor: 'pointer',
-                                    display: 'block',
-                                    fontSize: '35px',
-                                  }}
-                                  onClick={() =>
-                                    window.open(`${BASE_URL}${n[key]}`)
-                                  }
-                                />
-                              ) : ['doc', 'docx'].includes(
-                                  n[key]?.split('.').pop()?.toLowerCase()
-                                ) ? (
-                                <DescriptionIcon
-                                  style={{
-                                    color: 'blue',
-                                    cursor: 'pointer',
-                                    display: 'block',
-                                    fontSize: '35px',
-                                  }}
-                                  onClick={() =>
-                                    window.open(`${BASE_URL}${n[key]}`)
-                                  }
-                                />
+                      {Object.entries(n).map(
+                        ([key, value]) =>
+                          key !== 'id' &&
+                          key !== 'random_number' && (
+                            <TableCell
+                              className='p-4 md:p-16 border-t-1 border-gray-200'
+                              component='th'
+                              scope='row'
+                              key={key}>
+                              {key === 'file' ? (
+                                n[key]?.split('.').pop()?.toLowerCase() ===
+                                'pdf' ? (
+                                  <PictureAsPdf
+                                    style={{
+                                      color: 'red',
+                                      cursor: 'pointer',
+                                      display: 'block',
+                                      fontSize: '35px',
+                                    }}
+                                    onClick={() =>
+                                      window.open(`${BASE_URL}${n[key]}`)
+                                    }
+                                  />
+                                ) : ['doc', 'docx'].includes(
+                                    n[key]?.split('.').pop()?.toLowerCase()
+                                  ) ? (
+                                  <DescriptionIcon
+                                    style={{
+                                      color: 'blue',
+                                      cursor: 'pointer',
+                                      display: 'block',
+                                      fontSize: '35px',
+                                    }}
+                                    onClick={() =>
+                                      window.open(`${BASE_URL}${n[key]}`)
+                                    }
+                                  />
+                                ) : (
+                                  <img
+                                    onClick={() =>
+                                      n.file &&
+                                      showImage(`${BASE_URL}${n[key]}`)
+                                    }
+                                    src={
+                                      n[key]
+                                        ? `${BASE_URL}${n[key]}`
+                                        : '/assets/images/logos/user.jpg'
+                                    }
+                                    style={{
+                                      height: '40px',
+                                      width: '40px',
+                                      borderRadius: '50%',
+                                    }}
+                                    alt='uploaded file'
+                                  />
+                                )
+                              ) : (key === 'created_at' ||
+                                  key === 'passport_issue_date') &&
+                                n[key] ? (
+                                moment(new Date(n[key])).format('DD-MM-YYYY')
+                              ) : (key === 'updated_at' ||
+                                  key === 'passport_issue_date') &&
+                                n[key] ? (
+                                moment(new Date(n[key])).format('DD-MM-YYYY')
+                              ) : (key === 'created_at' ||
+                                  key === 'passport_expiry_date') &&
+                                n[key] ? (
+                                moment(new Date(n[key])).format('DD-MM-YYYY')
+                              ) : (key === 'updated_at' ||
+                                  key === 'passport_expiry_date') &&
+                                n[key] ? (
+                                moment(new Date(n[key])).format('DD-MM-YYYY')
+                              ) : (key === 'is_debtor' || key === 'is_paid') &&
+                                n[key] !== undefined ? (
+                                n[key] ? (
+                                  'Yes'
+                                ) : (
+                                  'No'
+                                )
                               ) : (
-                                <img
-                                  onClick={() =>
-                                    n.file && showImage(`${BASE_URL}${n[key]}`)
-                                  }
-                                  src={
-                                    n[key]
-                                      ? `${BASE_URL}${n[key]}`
-                                      : 'assets/logos/user.jpg'
-                                  }
-                                  style={{
-                                    height: '40px',
-                                    width: '40px',
-                                    borderRadius: '50%',
-                                  }}
-                                  alt='uploaded file'
-                                />
-                              )
-                            ) : (key === 'created_at' ||
-                                key === 'passport_issue_date') &&
-                              n[key] ? (
-                              moment(new Date(n[key])).format('DD-MM-YYYY')
-                            ) : (key === 'updated_at' ||
-                                key === 'passport_issue_date') &&
-                              n[key] ? (
-                              moment(new Date(n[key])).format('DD-MM-YYYY')
-                            ) : (key === 'created_at' ||
-                                key === 'passport_expiry_date') &&
-                              n[key] ? (
-                              moment(new Date(n[key])).format('DD-MM-YYYY')
-                            ) : (key === 'updated_at' ||
-                                key === 'passport_expiry_date') &&
-                              n[key] ? (
-                              moment(new Date(n[key])).format('DD-MM-YYYY')
-                            ) : (key === 'is_debtor' || key === 'is_paid') &&
-                              n[key] !== undefined ? (
-                              n[key] ? (
-                                'Yes'
-                              ) : (
-                                'No'
-                              )
-                            ) : (
-                              value
-                            )}
-                          </TableCell>
-                        )
-                    )}
-
-                    <TableCell
-                      className='p-4 md:p-16 whitespace-nowrap border-t-1  border-gray-200'
-                      component='th'
-                      scope='row'
-                      align='right'
-                      style={{
-                        position: 'sticky',
-                        right: 0,
-                        zIndex: 1,
-                        backgroundColor: '#fff',
-                      }}>
-                      <PrintIcon
-                        className='cursor-pointer custom-print-icon-style text-3xl'
-                        onClick={() => printCvBankRef.current.doPrint(n)}
-                      />
-                      {hasPermission('CVBANK_UPDATE') && (
-                        <Edit
-                          onClick={() => handleUpdateCvBank(n, 'updateCvBank')}
-                          className='cursor-pointer custom-edit-icon-style'
-                        />
+                                value
+                              )}
+                            </TableCell>
+                          )
                       )}
 
-                      {hasPermission('CVBANK_DELETE') && (
-                        <Delete
-                          onClick={() => handleDeleteCvBank(n, 'deleteCvBank')}
-                          className='cursor-pointer custom-delete-icon-style'
+                      <TableCell
+                        className='p-4 md:p-16 whitespace-nowrap border-t-1  border-gray-200'
+                        component='th'
+                        scope='row'
+                        align='right'
+                        style={{
+                          position: 'sticky',
+                          right: 0,
+                          zIndex: 1,
+                          backgroundColor: '#fff',
+                        }}>
+                        <PrintIcon
+                          className='cursor-pointer custom-print-icon-style text-3xl'
+                          onClick={() => printCvBankRef.current.doPrint(n)}
                         />
-                      )}
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
+                        {hasPermission('CVBANK_UPDATE') && (
+                          <Edit
+                            onClick={() =>
+                              handleUpdateCvBank(n, 'updateCvBank')
+                            }
+                            className='cursor-pointer custom-edit-icon-style'
+                          />
+                        )}
+
+                        {hasPermission('CVBANK_DELETE') && (
+                          <Delete
+                            onClick={() =>
+                              handleDeleteCvBank(n, 'deleteCvBank')
+                            }
+                            className='cursor-pointer custom-delete-icon-style'
+                          />
+                        )}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
 
-      <div id='pagiContainer' className='flex justify-between mb-6'>
+      <div className={classes.root} id='pagiContainer'>
         <Pagination
           count={totalData?.total_pages}
           page={page + 1}
@@ -374,7 +415,7 @@ function CvBanksTable(props) {
         />
 
         <TablePagination
-          className='shrink-0 mb-2'
+          className='shrink-0'
           component='div'
           rowsPerPageOptions={rowsPerPageOptions}
           count={totalData?.total_elements}
