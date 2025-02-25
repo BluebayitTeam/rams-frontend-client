4b0cdafe5bd405716b571cd2d383610149e39cba
shuvo784 <mrmasudhossain@gmail.com>
2025-02-25 11:14:10 +0600

    fix: enhance image upload handling and validation in PassengerForm

--- src/app/main/rams/PassengersManagement/Passenger/PassengerForm.jsx
+++ src/app/main/rams/PassengersManagement/Passenger/PassengerForm.jsx
@@ -6,7 +6,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-alert */
 
-import { Autocomplete, Box, Icon, Typography } from '@mui/material';
+import { Autocomplete, Box, Icon, IconButton, Typography } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import { useEffect, useRef, useState } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
@@ -411,8 +411,6 @@ function PassengerForm(props) {
               <div className='flex flex-row justify-between w-full items-center'>
                 <label
                   htmlFor='button-file-1'
-                  style={{ boxShadow: '0px 0px 20px -10px}' }}
-                  label='Passport Picture'
                   className={clsx(
                     classes.productImageUpload,
                     'flex items-center justify-center relative w-80 h-60 rounded-12 overflow-hidden cursor-pointer hover:shadow-lg'
@@ -431,24 +429,21 @@ function PassengerForm(props) {
                       };
 
                       const file = e.target.files[0];
-
-                      setPassportImg(file?.file);
+                      setPassportImg(file);
                       onChange(file);
 
-                      if (file.size > 5134914) {
+                      if (file?.size > 5134914) {
                         document.getElementById(
                           'passportPicSizeValidation'
-                        ).innerText = 'image file size not more than 500 KB';
+                        ).innerText =
+                          'Image file size should not exceed 500 KB';
                       } else {
                         document.getElementById(
                           'passportPicSizeValidation'
                         ).innerText = '';
                         if (routeParams.passengerId === 'new') {
                           try {
-                            const res = await dispatch(
-                              createPassengerImage(file)
-                            );
-                            console.log(`knfdsf`, res);
+                            await dispatch(createPassengerImage(file));
                           } catch (error) {
                             console.error(
                               'Error occurred while creating passenger image:',
@@ -456,9 +451,7 @@ function PassengerForm(props) {
                             );
                           }
                         }
-                        reader.readAsDataURL(e.target.files[0]);
-
-                        onChange(selectImage(file));
+                        reader.readAsDataURL(file);
                       }
                     }}
                   />
@@ -470,6 +463,46 @@ function PassengerForm(props) {
               </div>
             )}
           />
+
+          <div className='flex flex-wrap w-full items-center mb-16'>
+            {/* Render Existing Image */}
+            {passportPic && !previewImage1 && (
+              <div
+                style={{ width: '70px', height: '70px', position: 'relative' }}>
+                <img src={`${BASE_URL}${passportPic}`} alt='not_found' />
+              </div>
+            )}
+
+            {/* Render Selected Image with Cross Icon */}
+            {previewImage1 && (
+              <div
+                style={{
+                  width: '70px',
+                  height: '70px',
+                  position: 'relative',
+                  display: 'flex',
+                  alignItems: 'center',
+                }}>
+                <img src={previewImage1} alt='Passport' />
+                <IconButton
+                  style={{
+                    position: 'absolute',
+                    top: '-25px',
+                    right: '-8px',
+                    color: 'red',
+                    borderRadius: '50%',
+                  }}
+                  size='small'
+                  onClick={() => {
+                    setPreviewImage1(null);
+                    setPassportImg(null);
+                    onChange(null); // Reset file input
+                  }}>
+                  <HighlightOffIcon fontSize='small' />
+                </IconButton>
+              </div>
+            )}
+          </div>
         </div>
         <p className='mb-5 text-red-700	' id='passportPicSizeValidation'></p>
       </div>
@@ -1390,7 +1423,7 @@ function PassengerForm(props) {
         }}
       />
       <div className='flex md:space-x-12 flex-col md:flex-row'>
-        {/* <div className='flex flex-wrap w-full   my-2 justify-evenly items-center'>
+        <div className='flex flex-wrap w-full   my-2 justify-evenly items-center'>
           {passportPic && !previewImage1 && (
             <div style={{ width: '100px', height: '100px' }}>
               <img src={`${BASE_URL}${passportPic}`} alt='not_found' />
@@ -1400,7 +1433,7 @@ function PassengerForm(props) {
           <div style={{ width: '100px', height: '100px' }}>
             <img label='Passport Picture' src={previewImage1} alt='' />
           </div>
-        </div> */}
+        </div>
 
         <div className='flex flex-wrap w-full   my-2 justify-evenly'>
           <Image
