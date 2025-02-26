d58e20ce3ff0e6790134eaffc92e836b82c1add2
shuvo784 <mrmasudhossain@gmail.com>
2025-02-26 18:25:40 +0600

    feat: enhance SignInPage with improved styling and animations

--- src/app/main/sign-in/SignInPage.jsx
+++ src/app/main/sign-in/SignInPage.jsx
@@ -5,7 +5,30 @@ import { useEffect, useState } from 'react';
 import CardContent from '@mui/material/CardContent';
 import { BASE_URL, GET_SITESETTINGS } from 'src/app/constant/constants';
 import JwtLoginTab from './tabs/JwtSignInTab';
+import { makeStyles } from '@mui/styles';
+import clsx from 'clsx';
+import { Card, darken } from '@mui/material';
+import { motion } from 'framer-motion';
+
 // import FirebaseSignInTab from './tabs/FirebaseSignInTab';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    background: `linear-gradient(to right, ${theme.palette.primary.dark} 0%, ${darken(
+      theme.palette.primary.dark,
+      0.5
+    )} 100%)`,
+    color: theme.palette.primary.contrastText,
+  },
+  leftSection: {},
+  rightSection: {
+    background: `linear-gradient(to right, ${theme.palette.primary.dark} 0%, ${darken(
+      theme.palette.primary.dark,
+      0.5
+    )} 100%)`,
+    color: theme.palette.primary.contrastText,
+  },
+}));
 
 const tabs = [
   {
@@ -20,6 +43,8 @@ const tabs = [
  * The sign in page.
  */
 function SignInPage() {
+  const classes = useStyles();
+
   const [selectedTabId, setSelectedTabId] = useState(tabs[0].id);
 
   function handleSelectTab(id) {
@@ -27,6 +52,7 @@ function SignInPage() {
   }
 
   const [generalData, setGeneralData] = useState({});
+  console.log('generalData', generalData);
 
   // get general setting data
   useEffect(() => {
@@ -35,94 +61,63 @@ function SignInPage() {
       .then((data) => setGeneralData(data?.general_settings[0] || {}));
   }, []);
   return (
-    <div className='flex min-w-0 flex-1 flex-col items-center sm:flex-row sm:justify-center md:items-start md:justify-start'>
-      <Paper
-        className='h-full w-full px-16 py-8 ltr:border-r-1 rtl:border-l-1 sm:h-auto sm:w-auto sm:rounded-2xl sm:p-48 sm:shadow md:flex md:h-full md:w-1/2 md:items-center md:justify-end md:rounded-none md:p-64 md:shadow-none'
-        style={{ justifyContent: 'center' }}>
-        <CardContent className='mx-auto  w-1/2 sm:mx-0 text-center whitespace-nowrap'>
-          <div className='flex items-center mb-48'>
-            <img
-              className='logo-icon w-48'
-              src={`${BASE_URL}${generalData?.favicon}`}
-              alt='logo'
-            />
-            <div className='mr-4  h-40' />
-            <div>
-              <Typography
-                className='text-24 font-semibold logo-text whitespace-nowrap'
-                color='inherit'>
-                {generalData?.site_name}
-              </Typography>
-              <Typography
-                className='text-16 tracking-widest -mt-8 font-700'
-                color='textSecondary'>
-                {/* REACT */}
-              </Typography>
-            </div>
-          </div>
+    <div
+      className={clsx(
+        classes.root,
+        'flex flex-col flex-auto items-center justify-center flex-shrink-0 p-16 md:p-24'
+      )}>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.6 }}
+        animate={{ opacity: 1, scale: 1 }}
+        className='flex w-full max-w-[400px] md:max-w-3xl rounded-20 shadow-2xl overflow-hidden'>
+        <Card
+          className={clsx(
+            classes.leftSection,
+            'flex flex-col w-full max-w-sm items-center justify-center shadow-0'
+          )}
+          square>
+          <CardContent className='flex flex-col items-center justify-center w-full py-[96px] max-w-[320px]'>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, transition: { delay: 0.2 } }}>
+              <div className='flex items-center mb-[48px]'>
+                <img
+                  className='logo-icon w-[48px]'
+                  src='../../../../public/assets/images/logo/BLUEBAYITLOGO.png'
+                  alt='logo'
+                />
+                <div className=' mr-4 w-1 h-[40px]' />
+                <div>
+                  <Typography
+                    className='text-20 font-semibold logo-text'
+                    color='inherit'>
+                    {generalData?.site_name}
+                  </Typography>
+                </div>
+              </div>
+            </motion.div>
 
-          {selectedTabId === 'jwt' && <JwtLoginTab />}
-          {/* {selectedTabId === 'firebase' && <FirebaseSignInTab />} */}
-        </CardContent>
-      </Paper>
+            {selectedTabId === 'jwt' && <JwtLoginTab />}
+          </CardContent>
+        </Card>
 
-      <Box
-        className='relative hidden h-full flex-auto items-center justify-center overflow-hidden p-64 md:flex lg:px-112'
-        sx={{ backgroundColor: 'primary.main' }}>
-        <svg
-          className='pointer-events-none absolute inset-0'
-          viewBox='0 0 960 540'
-          width='100%'
-          height='100%'
-          preserveAspectRatio='xMidYMax slice'
-          xmlns='http://www.w3.org/2000/svg'>
-          <Box
-            component='g'
-            sx={{ color: 'primary.light' }}
-            className='opacity-20'
-            fill='none'
-            stroke='currentColor'
-            strokeWidth='100'>
-            <circle r='234' cx='196' cy='23' />
-            <circle r='234' cx='790' cy='491' />
-          </Box>
-        </svg>
-        <Box
-          component='svg'
-          className='absolute -right-64 -top-64 opacity-20'
-          sx={{ color: 'primary.light' }}
-          viewBox='0 0 220 192'
-          width='220px'
-          height='192px'
-          fill='none'>
-          <defs>
-            <pattern
-              id='837c3e70-6c3a-44e6-8854-cc48c737b659'
-              x='0'
-              y='0'
-              width='20'
-              height='20'
-              patternUnits='userSpaceOnUse'>
-              <rect x='0' y='0' width='4' height='4' fill='currentColor' />
-            </pattern>
-          </defs>
-          <rect
-            width='220'
-            height='192'
-            fill='url(#837c3e70-6c3a-44e6-8854-cc48c737b659)'
-          />
-        </Box>
-
-        <div className='hidden md:flex flex-1 items-center justify-center p-64'>
-          <div className='max-w-320'>
-            <img
-              src={`${BASE_URL}${generalData?.rams_logo}`}
-              className='text-24 font-semibold logo-text'
-              alt='Logo'
-            />
+        <div
+          className={clsx(
+            classes.rightSection,
+            'hidden md:flex flex-1 items-center justify-center p-[64px]'
+          )}>
+          <div className='max-w-[320px]'>
+            <motion.div
+              initial={{ opacity: 0, y: 40 }}
+              animate={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}>
+              <img
+                src='../../../../public/assets/images/logos/bbit.png'
+                className='text-64 font-semibold logo-text'
+              />
+            </motion.div>
           </div>
         </div>
-      </Box>
+      </motion.div>
     </div>
   );
 }
