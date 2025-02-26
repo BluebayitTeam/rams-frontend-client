127e87250ed2b216c86825f60e65ef6304f93588
shuvo784 <mrmasudhossain@gmail.com>
2025-02-26 12:03:31 +0600

    fix: refactor QualificationsTableHead for improved readability and sticky header functionality

--- src/app/main/rams/QualificationsManagement/qualifications/QualificationsTableHead.jsx
+++ src/app/main/rams/QualificationsManagement/qualifications/QualificationsTableHead.jsx
@@ -13,144 +13,164 @@ import { useDeleteQualificationsMutation } from '../QualificationsApi';
  */
 
 const rows = [
-	{
-		id: 'SL',
-		align: 'left',
-		disablePadding: true,
-		label: 'SL',
-		sort: true
-	},
-	{
-		id: 'name',
-		align: 'left',
-		disablePadding: false,
-		label: 'Name',
-		sort: true
-	},
-	{
-		id: 'degree_name',
-		align: 'left',
-		disablePadding: false,
-		label: 'Degree Name',
-		sort: true
-	},
-	{
-		id: 'passign_year',
-		align: 'left',
-		disablePadding: false,
-		label: 'Passign Year',
-		sort: true
-	},
-	{
-		id: 'board',
-		align: 'left',
-		disablePadding: false,
-		label: 'Board',
-		sort: true
-	},
-	{
-		id: 'institute_name',
-		align: 'left',
-		disablePadding: false,
-		label: 'Institute Name',
-		sort: true
-	},
-	{
-		id: 'grade',
-		align: 'left',
-		disablePadding: false,
-		label: 'Grade',
-		sort: true
-	},
-	{
-		id: 'action',
-		align: 'center',
-		disablePadding: false,
-		label: 'Action',
-		sort: true
-	}
+  {
+    id: 'SL',
+    align: 'left',
+    disablePadding: true,
+    label: 'SL',
+    sort: true,
+  },
+  {
+    id: 'name',
+    align: 'left',
+    disablePadding: false,
+    label: 'Name',
+    sort: true,
+  },
+  {
+    id: 'degree_name',
+    align: 'left',
+    disablePadding: false,
+    label: 'Degree Name',
+    sort: true,
+  },
+  {
+    id: 'passign_year',
+    align: 'left',
+    disablePadding: false,
+    label: 'Passign Year',
+    sort: true,
+  },
+  {
+    id: 'board',
+    align: 'left',
+    disablePadding: false,
+    label: 'Board',
+    sort: true,
+  },
+  {
+    id: 'institute_name',
+    align: 'left',
+    disablePadding: false,
+    label: 'Institute Name',
+    sort: true,
+  },
+  {
+    id: 'grade',
+    align: 'left',
+    disablePadding: false,
+    label: 'Grade',
+    sort: true,
+  },
+  {
+    id: 'action',
+    align: 'center',
+    disablePadding: false,
+    label: 'Action',
+    sort: true,
+  },
 ];
 
 function QualificationsTableHead(props) {
-	const { selectedQualificationIds, tableOrder, onSelectAllClick, onRequestSort, rowCount, onMenuItemClick } = props;
+  const {
+    selectedQualificationIds,
+    tableOrder,
+    onSelectAllClick,
+    onRequestSort,
+    rowCount,
+    onMenuItemClick,
+  } = props;
 
-	console.log('onMenuItemClick', onMenuItemClick);
+  console.log('onMenuItemClick', onMenuItemClick);
 
-	console.log('selectedQualificationIds', selectedQualificationIds);
+  console.log('selectedQualificationIds', selectedQualificationIds);
 
-	const [removeQualifications] = useDeleteQualificationsMutation();
-	const numSelected = selectedQualificationIds.length;
-	const [selectedQualificationsMenu, setSelectedQualificationsMenu] = useState(null);
-	const createSortHandler = (event, property) => {
-		onRequestSort(event, property);
-	};
+  const [removeQualifications] = useDeleteQualificationsMutation();
+  const numSelected = selectedQualificationIds.length;
+  const [selectedQualificationsMenu, setSelectedQualificationsMenu] =
+    useState(null);
+  const createSortHandler = (event, property) => {
+    onRequestSort(event, property);
+  };
 
-	function openSelectedQualificationsMenu(event) {
-		setSelectedQualificationsMenu(event.currentTarget);
-	}
+  function openSelectedQualificationsMenu(event) {
+    setSelectedQualificationsMenu(event.currentTarget);
+  }
 
-	function closeSelectedQualificationsMenu() {
-		setSelectedQualificationsMenu(null);
-	}
+  function closeSelectedQualificationsMenu() {
+    setSelectedQualificationsMenu(null);
+  }
 
-	function handleDeleteMultipleItem() {
-		removeQualifications(selectedQualificationIds).then((data) => {
-			Swal.fire({
-				position: 'top-center',
-				icon: 'success',
-				title: 'Deleted Successfully',
-				showConfirmButton: false,
-				timer: 2000
-			});
-		});
-	}
+  function handleDeleteMultipleItem() {
+    removeQualifications(selectedQualificationIds).then((data) => {
+      Swal.fire({
+        position: 'top-center',
+        icon: 'success',
+        title: 'Deleted Successfully',
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    });
+  }
 
-	return (
-		<TableHead>
-			<TableRow className="h-48 sm:h-64">
-				{rows.map((row, index, array) => {
-					return (
-						<TableCell
-							sx={{
-								backgroundColor: (theme) =>
-									theme.palette.mode === 'light'
-										? lighten(theme.palette.background.default, 0.4)
-										: lighten(theme.palette.background.default, 0.02)
-							}}
-							className="p-4 md:p-16 whitespace-nowrap"
-							key={row.id}
-							align={row.align}
-							padding={row.disablePadding ? 'none' : 'normal'}
-							style={{
-								position: index === 0 || index === array.length - 1 ? 'sticky' : 'inherit',
-								left: index === 0 ? 0 : undefined,
-								right: index === array.length - 1 ? 0 : undefined,
-								zIndex: index === 0 || index === array.length - 1 ? 1 : 'auto'
-							}}
-							sortDirection={tableOrder.id === row.id ? tableOrder.direction : false}
-						>
-							{row.sort && (
-								<Tooltip
-									title="Sort"
-									placement={row.align === 'right' ? 'bottom-end' : 'bottom-start'}
-									enterDelay={300}
-								>
-									<TableSortLabel
-										active={tableOrder.id === row.id}
-										direction={tableOrder.direction}
-										onClick={(ev) => createSortHandler(ev, row.id)}
-										className="font-semibold"
-									>
-										{row.label}
-									</TableSortLabel>
-								</Tooltip>
-							)}
-						</TableCell>
-					);
-				})}
-			</TableRow>
-		</TableHead>
-	);
+  return (
+    <TableHead
+      sx={{
+        position: 'sticky',
+        top: 0,
+        zIndex: 10,
+        backgroundColor: (theme) =>
+          theme.palette.mode === 'light'
+            ? lighten(theme.palette.background.default, 0.4)
+            : lighten(theme.palette.background.default, 0.02),
+      }}>
+      <TableRow className='h-48 sm:h-64'>
+        {rows.map((row, index, array) => {
+          return (
+            <TableCell
+              sx={{
+                position:
+                  index === 0 || index === array.length - 1
+                    ? 'sticky'
+                    : 'inherit',
+                left: index === 0 ? 0 : undefined,
+                right: index === array.length - 1 ? 0 : undefined,
+                zIndex: index === 0 || index === array.length - 1 ? 2 : 'auto',
+                backgroundColor: (theme) =>
+                  theme.palette.mode === 'light'
+                    ? lighten(theme.palette.background.default, 0.4)
+                    : lighten(theme.palette.background.default, 0.02),
+                fontWeight: 'bold',
+              }}
+              className='p-4 md:p-16  whitespace-nowrap'
+              key={row.id}
+              align={row.align}
+              padding={row.disablePadding ? 'none' : 'normal'}
+              sortDirection={
+                tableOrder.id === row.id ? tableOrder.direction : false
+              }>
+              {row.sort && (
+                <Tooltip
+                  title='Sort'
+                  placement={
+                    row.align === 'right' ? 'bottom-end' : 'bottom-start'
+                  }
+                  enterDelay={300}>
+                  <TableSortLabel
+                    active={tableOrder.id === row.id}
+                    direction={tableOrder.direction}
+                    onClick={(ev) => createSortHandler(ev, row.id)}
+                    className='font-semibold'>
+                    {row.label}
+                  </TableSortLabel>
+                </Tooltip>
+              )}
+            </TableCell>
+          );
+        })}
+      </TableRow>
+    </TableHead>
+  );
 }
 
 export default QualificationsTableHead;
