757201d6a8cd242dcf71746abc4ebc10f7e5675b
shuvo784 <mrmasudhossain@gmail.com>
2025-02-27 11:11:39 +0600

    feat: enhance Notification components with improved layout and notification count display

--- src/app/main/apps/notifications/NotificationPanelToggleButton.jsx
+++ src/app/main/apps/notifications/NotificationPanelToggleButton.jsx
@@ -11,24 +11,30 @@ import { useGetAllNotificationsQuery } from './NotificationApi';
  * The notification panel toggle button.
  */
 function NotificationPanelToggleButton(props) {
-	const { children = <FuseSvgIcon>heroicons-outline:bell</FuseSvgIcon> } = props;
-	const { data: notifications } = useGetAllNotificationsQuery();
-	const dispatch = useAppDispatch();
-	return (
-		<IconButton
-			className="h-40 w-40"
-			onClick={() => dispatch(toggleNotificationPanel())}
-			size="large"
-		>
-			<Badge
-				color="secondary"
-				variant="dot"
-				invisible={notifications?.length === 0}
-			>
-				{children}
-			</Badge>
-		</IconButton>
-	);
+  const { children = <FuseSvgIcon>heroicons-outline:bell</FuseSvgIcon> } =
+    props;
+  const { data: notifications } = useGetAllNotificationsQuery();
+  const dispatch = useAppDispatch();
+  return (
+    <IconButton
+      className='h-40 w-40'
+      onClick={() => dispatch(toggleNotificationPanel())}
+      size='large'>
+      <Badge color='secondary' invisible={notifications?.length === 0}>
+        {children}
+      </Badge>
+      <span class='absolute inset-0 object-right-top -mr-6'>
+        <button
+          type='button'
+          class=' ml-16 inline-block px-3 py-2 bg-blue-200  font-medium text-xs leading-tight uppercase rounded-full '>
+          {notifications?.total_count ? notifications?.total_count : 0}
+        </button>
+      </span>
+    </IconButton>
+  );
 }
 
-export default withReducer('notificationPanel', reducer)(NotificationPanelToggleButton);
+export default withReducer(
+  'notificationPanel',
+  reducer
+)(NotificationPanelToggleButton);
