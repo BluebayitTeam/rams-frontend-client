2bc86aea49e91dd0f42f4e9aaf34a90ac4635bdc
shuvo784 <mrmasudhossain@gmail.com>
2025-02-27 15:53:34 +0600

    feat: enhance MedicalApi and MedicalHeader with improved notification handling and API integration

--- src/app/main/rams/MedicalsManagement/MedicalsApi.js
+++ src/app/main/rams/MedicalsManagement/MedicalsApi.js
@@ -1,73 +1,78 @@
 import { apiService as api } from 'app/store/apiService';
 import { createSelector } from '@reduxjs/toolkit';
 import FuseUtils from '@fuse/utils';
-import { MEDICAL_BY_PASSENGER_ID, CREATE_MEDICAL, UPDATE_MEDICAL, DELETE_MEDICAL } from 'src/app/constant/constants';
+import {
+  MEDICAL_BY_PASSENGER_ID,
+  CREATE_MEDICAL,
+  UPDATE_MEDICAL,
+  DELETE_MEDICAL,
+} from 'src/app/constant/constants';
 import jsonToFormData from 'src/app/@helpers/jsonToFormData';
 import { selectSearchText } from './store/searchTextSlice';
 import MedicalModel from './medical/models/MedicalModel';
 
 export const addTagTypes = ['medicals'];
 const MedicalApi = api
-	.enhanceEndpoints({
-		addTagTypes
-	})
-	.injectEndpoints({
-		endpoints: (build) => ({
-			getMedical: build.query({
-				query: (medicalId) => ({
-					url: `${MEDICAL_BY_PASSENGER_ID}${medicalId}`
-				}),
-				providesTags: ['medicals']
-			}),
-			createMedical: build.mutation({
-				query: (newMedical) => ({
-					url: CREATE_MEDICAL,
-					method: 'POST',
-					data: jsonToFormData(
-						MedicalModel({
-							...newMedical
-							// medical_exam_date: moment(new Date(newMedical?.medical_exam_date)).format('YYYY-MM-DD')
-						})
-					)
-				}),
-				invalidatesTags: ['medicals']
-			}),
-			updateMedical: build.mutation({
-				query: (medical) => ({
-					url: `${UPDATE_MEDICAL}${medical.id}`,
-					method: 'PUT',
-					data: jsonToFormData({
-						...medical
-						// medical_exam_date: moment(new Date(medical?.medical_exam_date)).format('YYYY-MM-DD')
-					})
-				}),
-				invalidatesTags: ['medicals']
-			}),
-			deleteMedical: build.mutation({
-				query: (medicalId) => ({
-					url: `${DELETE_MEDICAL}${medicalId}`,
-					method: 'DELETE'
-				}),
-				invalidatesTags: ['medicals']
-			})
-		}),
-		overrideExisting: false
-	});
+  .enhanceEndpoints({
+    addTagTypes,
+  })
+  .injectEndpoints({
+    endpoints: (build) => ({
+      getMedical: build.query({
+        query: (medicalId) => ({
+          url: `${MEDICAL_BY_PASSENGER_ID}${medicalId}`,
+        }),
+        providesTags: ['medicals'],
+      }),
+      createMedical: build.mutation({
+        query: (newMedical) => ({
+          url: CREATE_MEDICAL,
+          method: 'POST',
+          data: jsonToFormData(
+            MedicalModel({
+              ...newMedical,
+              // medical_exam_date: moment(new Date(newMedical?.medical_exam_date)).format('YYYY-MM-DD')
+            })
+          ),
+        }),
+        invalidatesTags: ['medicals'],
+      }),
+      updateMedical: build.mutation({
+        query: (medical) => ({
+          url: `${UPDATE_MEDICAL}${medical.id}`,
+          method: 'PUT',
+          data: jsonToFormData({
+            ...medical,
+            // medical_exam_date: moment(new Date(medical?.medical_exam_date)).format('YYYY-MM-DD')
+          }),
+        }),
+        invalidatesTags: ['medicals'],
+      }),
+      deleteMedical: build.mutation({
+        query: (medicalId) => ({
+          url: `${DELETE_MEDICAL}${medicalId}`,
+          method: 'DELETE',
+        }),
+        invalidatesTags: ['medicals'],
+      }),
+    }),
+    overrideExisting: false,
+  });
 export default MedicalApi;
 export const {
-	useGetMedicalsQuery,
-	useDeleteMedicalsMutation,
-	useGetMedicalQuery,
-	useUpdateMedicalMutation,
-	useDeleteMedicalMutation,
-	useCreateMedicalMutation
+  useGetMedicalsQuery,
+  useDeleteMedicalsMutation,
+  useGetMedicalQuery,
+  useUpdateMedicalMutation,
+  useDeleteMedicalMutation,
+  useCreateMedicalMutation,
 } = MedicalApi;
 
 export const selectFilteredMedicals = (medicals) =>
-	createSelector([selectSearchText], (searchText) => {
-		if (searchText?.length === 0) {
-			return medicals;
-		}
+  createSelector([selectSearchText], (searchText) => {
+    if (searchText?.length === 0) {
+      return medicals;
+    }
 
-		return FuseUtils.filterArrayByString(medicals, searchText);
-	});
+    return FuseUtils.filterArrayByString(medicals, searchText);
+  });
