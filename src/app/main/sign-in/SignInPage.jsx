d1b2d312bbf55677235376f23cc7373e0eeb6c29
shuvo784 <mrmasudhossain@gmail.com>
2025-02-27 09:43:54 +0600

    feat: update SignInPage layout and styling for improved responsiveness and visual appeal

--- src/app/main/sign-in/SignInPage.jsx
+++ src/app/main/sign-in/SignInPage.jsx
@@ -64,54 +64,55 @@ function SignInPage() {
     <div
       className={clsx(
         classes.root,
-        'flex flex-col flex-auto items-center justify-center flex-shrink-0 p-16 md:p-24'
+        'flex flex-col items-center justify-center flex-shrink-0 p-16 md:p-32 w-full min-h-screen'
       )}>
       <motion.div
         initial={{ opacity: 0, scale: 0.6 }}
         animate={{ opacity: 1, scale: 1 }}
-        className='flex w-full max-w-[400px] md:max-w-3xl rounded-20 shadow-2xl overflow-hidden'>
+        className='flex w-full max-w-none rounded-20 shadow-2xl overflow-hidden'>
+        {/* Left Section */}
         <Card
           className={clsx(
             classes.leftSection,
-            'flex flex-col w-full max-w-sm items-center justify-center shadow-0'
+            'flex flex-col flex-1 items-center justify-center shadow-0 w-full'
           )}
           square>
-          <CardContent className='flex flex-col items-center justify-center w-full py-[96px] max-w-[320px]'>
+          <CardContent className='flex flex-col items-center justify-center w-full py-[96px] max-w-[400px]'>
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1, transition: { delay: 0.2 } }}>
-              <div className='flex items-center mb-[48px]'>
+              <div className='flex items-center mb-[28px]'>
                 <img
-                  className='logo-icon w-[48px]'
-                  src='../../../../public/assets/images/logo/BLUEBAYITLOGO.png'
+                  className='logo-icon w-[64px]'
+                  src='/assets/images/logo/BLUEBAYITLOGO.png'
                   alt='logo'
                 />
-                <div className=' mr-4 w-1 h-[40px]' />
+                <div className='mr-4 w-1 h-[40px]' />
                 <div>
                   <Typography
-                    className='text-20 font-semibold logo-text'
+                    className='text-3xl font-semibold logo-text'
                     color='inherit'>
                     {generalData?.site_name}
                   </Typography>
                 </div>
               </div>
+              {selectedTabId === 'jwt' && <JwtLoginTab />}
             </motion.div>
-
-            {selectedTabId === 'jwt' && <JwtLoginTab />}
           </CardContent>
         </Card>
 
+        {/* Right Section */}
         <div
           className={clsx(
             classes.rightSection,
-            'hidden md:flex flex-1 items-center justify-center p-[64px]'
+            'flex flex-1 items-center justify-center p-[64px] w-full'
           )}>
           <div className='max-w-[320px]'>
             <motion.div
               initial={{ opacity: 0, y: 40 }}
               animate={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}>
               <img
-                src='../../../../public/assets/images/logos/bbit.png'
+                src='/assets/images/logos/bbit.png'
                 className='text-64 font-semibold logo-text'
               />
             </motion.div>
