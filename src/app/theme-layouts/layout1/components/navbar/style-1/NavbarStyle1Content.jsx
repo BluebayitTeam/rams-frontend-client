d6a39fbb95f7451ada080e8094b09b1c5ccd1c6f
shuvo784 <mrmasudhossain@gmail.com>
2025-03-05 15:11:12 +0600

    Refactor NavbarStyle1Content to use MUI's AppBar and makeStyles for improved styling and structure

--- src/app/theme-layouts/layout1/components/navbar/style-1/NavbarStyle1Content.jsx
+++ src/app/theme-layouts/layout1/components/navbar/style-1/NavbarStyle1Content.jsx
@@ -6,60 +6,75 @@ import Navigation from 'app/theme-layouts/shared-components/navigation/Navigatio
 import NavbarToggleButton from 'app/theme-layouts/shared-components/navbar/NavbarToggleButton';
 import Logo from '../../../../shared-components/Logo';
 import UserNavbarHeader from '../../../../shared-components/UserNavbarHeader';
+import { makeStyles } from '@mui/styles';
+import { AppBar } from '@mui/material';
+import { useTheme } from '@emotion/react';
 
-const Root = styled('div')(({ theme }) => ({
-	backgroundColor: theme.palette.background.default,
-	color: theme.palette.text.primary,
-	'& ::-webkit-scrollbar-thumb': {
-		boxShadow: `inset 0 0 0 20px ${theme.palette.mode === 'light' ? 'rgba(0, 0, 0, 0.24)' : 'rgba(255, 255, 255, 0.24)'}`
-	},
-	'& ::-webkit-scrollbar-thumb:active': {
-		boxShadow: `inset 0 0 0 20px ${theme.palette.mode === 'light' ? 'rgba(0, 0, 0, 0.37)' : 'rgba(255, 255, 255, 0.37)'}`
-	}
-}));
-const StyledContent = styled(FuseScrollbars)(() => ({
-	overscrollBehavior: 'contain',
-	overflowX: 'hidden',
-	overflowY: 'auto',
-	WebkitOverflowScrolling: 'touch',
-	backgroundRepeat: 'no-repeat',
-	backgroundSize: '100% 40px, 100% 10px',
-	backgroundAttachment: 'local, scroll'
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.default,
+    color: theme.palette.text.primary,
+    '& ::-webkit-scrollbar-thumb': {
+      boxShadow: `inset 0 0 0 20px ${
+        theme.palette.type === 'light'
+          ? 'rgba(0, 0, 0, 0.24)'
+          : 'rgba(255, 255, 255, 0.24)'
+      }`,
+    },
+    '& ::-webkit-scrollbar-thumb:active': {
+      boxShadow: `inset 0 0 0 20px ${
+        theme.palette.type === 'light'
+          ? 'rgba(0, 0, 0, 0.37)'
+          : 'rgba(255, 255, 255, 0.37)'
+      }`,
+    },
+  },
+  content: {
+    overscrollBehavior: 'contain',
+    overflowX: 'hidden',
+    overflowY: 'auto',
+    '-webkit-overflow-scrolling': 'touch',
+    background:
+      'linear-gradient(rgba(0, 0, 0, 0) 30%, rgba(0, 0, 0, 0) 30%), linear-gradient(rgba(0, 0, 0, 0.25) 0, rgba(0, 0, 0, 0) 40%)',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: '100% 40px, 100% 10px',
+    backgroundAttachment: 'local, scroll',
+  },
 }));
 
 /**
  * The navbar style 1 content.
  */
 function NavbarStyle1Content(props) {
-	const { className = '' } = props;
-	return (
-		<Root className={clsx('flex h-full flex-auto flex-col overflow-hidden', className)}>
-			<div className="flex h-48 shrink-0 flex-row items-center px-20 md:h-20">
-				<div className="mx-4 flex flex-1">
-					<Logo />
-				</div>
+  const classes = useStyles();
+  const theme = useTheme();
+  return (
+    <div
+      className={clsx(
+        'flex flex-auto flex-col overflow-hidden h-full',
+        classes.root,
+        props.className
+      )}>
+      <AppBar
+        color='primary'
+        position='static'
+        className='flex flex-row items-center flex-shrink h-48 md:h-64 min-h-48 md:min-h-64 px-12 shadow-0'>
+        <div className='flex flex-1 mx-4'>
+          <Logo />
+        </div>
 
-				<NavbarToggleButton className="h-40 w-40 p-0" />
-			</div>
+        <NavbarToggleButton className='w-40 h-40 p-0' />
+      </AppBar>
 
-			<StyledContent
-				className="flex min-h-0 flex-1 flex-col"
-				option={{ suppressScrollX: true, wheelPropagation: false }}
-			>
-				<UserNavbarHeader />
+      <FuseScrollbars
+        className={clsx(classes.content)}
+        option={{ suppressScrollX: true, wheelPropagation: false }}>
+        <UserNavbarHeader />
 
-				<Navigation layout="vertical" />
-
-				<div className="flex-0 flex items-center justify-center py-48 opacity-10">
-					<img
-						className="w-full max-w-64"
-						src="assets/images/logo/bbit.png"
-						alt="footer logo"
-					/>
-				</div>
-			</StyledContent>
-		</Root>
-	);
+        <Navigation layout='vertical' />
+      </FuseScrollbars>
+    </div>
+  );
 }
 
 export default memo(NavbarStyle1Content);
