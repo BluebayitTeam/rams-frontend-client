232e1a804dae7e1ce5d237b6907f8a0f5665c00e
shuvo784 <mrmasudhossain@gmail.com>
2025-03-06 15:30:40 +0600

    refactor: improve styling and validation in CountryForm and Country components

--- src/app/main/rams/CountryssManagement/country/CountryForm.jsx
+++ src/app/main/rams/CountryssManagement/country/CountryForm.jsx
@@ -3,33 +3,36 @@ import { Controller, useFormContext } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 
 function CountryForm(props) {
-	const dispatch = useDispatch();
-	const methods = useFormContext();
-	const { control, formState, watch } = methods;
-	const { errors } = formState;
+  const dispatch = useDispatch();
+  const methods = useFormContext();
+  const { control, formState, watch } = methods;
+  const { errors } = formState;
 
-	return (
-		<div>
-			<Controller
-				name="name"
-				control={control}
-				render={({ field }) => (
-					<TextField
-						{...field}
-						className="mt-8 mb-16"
-						required
-						label="Name"
-						autoFocus
-						id="name"
-						variant="outlined"
-						fullWidth
-						error={!!errors.name}
-						helperText={errors?.name?.message}
-					/>
-				)}
-			/>
-		</div>
-	);
+  return (
+    <div>
+      <Controller
+        name='name'
+        control={control}
+        render={({ field }) => (
+          <TextField
+            {...field}
+            className='mt-8 mb-16'
+            required
+            label='Name'
+            autoFocus
+            id='name'
+            variant='outlined'
+            fullWidth
+            error={!!errors.name}
+            helperText={errors?.name?.message}
+            InputLabelProps={
+              field.value ? { shrink: true } : { style: { color: 'red' } }
+            }
+          />
+        )}
+      />
+    </div>
+  );
 }
 
 export default CountryForm;
