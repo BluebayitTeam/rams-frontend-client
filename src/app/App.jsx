8918a4cc50a0f4a98ea4ef1111e2ca7a3fe83d74
shuvo784 <mrmasudhossain@gmail.com>
2025-03-10 10:57:17 +0600

    refactor: integrate react-helmet and react-helmet-async for dynamic document head management

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet-async';
 import FuseLayout from '@fuse/core/FuseLayout';
 import FuseTheme from '@fuse/core/FuseTheme';
 import { SnackbarProvider } from 'notistack';
@@ -11,63 +12,68 @@ import { selectMainTheme } from '@fuse/core/FuseSettings/store/fuseSettingsSlice
 import MockAdapterProvider from '@mock-api/MockAdapterProvider';
 import withAppProviders from './withAppProviders';
 import { AuthRouteProvider } from './auth/AuthRouteProvider';
-// import axios from 'axios';
-/**
- * Axios HTTP Request defaults
- */
-// axios.defaults.baseURL = "";
-// axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-// axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded';
+import { useEffect, useState } from 'react';
+import { BASE_URL, GET_SITESETTINGS } from './constant/constants';
+
 const emotionCacheOptions = {
-	rtl: {
-		key: 'muirtl',
-		stylisPlugins: [rtlPlugin],
-		insertionPoint: document.getElementById('emotion-insertion-point')
-	},
-	ltr: {
-		key: 'muiltr',
-		stylisPlugins: [],
-		insertionPoint: document.getElementById('emotion-insertion-point')
-	}
+  rtl: {
+    key: 'muirtl',
+    stylisPlugins: [rtlPlugin],
+    insertionPoint: document.getElementById('emotion-insertion-point'),
+  },
+  ltr: {
+    key: 'muiltr',
+    stylisPlugins: [],
+    insertionPoint: document.getElementById('emotion-insertion-point'),
+  },
 };
 
-/**
- * The main App component.
- */
 function App() {
-	/**
-	 * The language direction from the Redux store.
-	 */
-	const langDirection = useSelector(selectCurrentLanguageDirection);
-	/**
-	 * The main theme from the Redux store.
-	 */
-	const mainTheme = useSelector(selectMainTheme);
-	return (
-		<MockAdapterProvider>
-			<CacheProvider value={createCache(emotionCacheOptions[langDirection])}>
-				<FuseTheme
-					theme={mainTheme}
-					direction={langDirection}
-				>
-					<AuthRouteProvider>
-						<SnackbarProvider
-							maxSnack={5}
-							anchorOrigin={{
-								vertical: 'bottom',
-								horizontal: 'right'
-							}}
-							classes={{
-								containerRoot: 'bottom-0 right-0 mb-52 md:mb-68 mr-8 lg:mr-80 z-99'
-							}}
-						>
-							<FuseLayout layouts={themeLayouts} />
-						</SnackbarProvider>
-					</AuthRouteProvider>
-				</FuseTheme>
-			</CacheProvider>
-		</MockAdapterProvider>
-	);
+  const langDirection = useSelector(selectCurrentLanguageDirection);
+  const mainTheme = useSelector(selectMainTheme);
+
+  const [generalData, setGeneralData] = useState({});
+  const [data, setData] = useState({});
+  console.log('FaviconURL', `${BASE_URL}${generalData?.favicon}`);
+
+  // Get general setting data
+  useEffect(() => {
+    fetch(`${GET_SITESETTINGS}`)
+      .then((response) => response.json())
+      .then((data) => setGeneralData(data.general_settings[0] || {}))
+      .catch(() => setGeneralData({}));
+  }, []);
+  return (
+    <MockAdapterProvider>
+      <CacheProvider value={createCache(emotionCacheOptions[langDirection])}>
+        <FuseTheme theme={mainTheme} direction={langDirection}>
+          <AuthRouteProvider>
+            <Helmet>
+              <title>{generalData?.title}</title>
+              <meta name='description' content='This is my dynamic React app' />
+              <link
+                rel='icon'
+                type='image/png'
+                href={`${BASE_URL}${generalData?.favicon}`}
+              />
+            </Helmet>
+            <SnackbarProvider
+              maxSnack={5}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+              }}
+              classes={{
+                containerRoot:
+                  'bottom-0 right-0 mb-52 md:mb-68 mr-8 lg:mr-80 z-99',
+              }}>
+              <FuseLayout layouts={themeLayouts} />
+            </SnackbarProvider>
+          </AuthRouteProvider>
+        </FuseTheme>
+      </CacheProvider>
+    </MockAdapterProvider>
+  );
 }
 
 export default withAppProviders(App);
