449c6f040e38bffad41709cf5dfd90901d9d529a
tonmoy-dev <rxdevs100@gmail.com>
2025-03-11 15:57:46 +0600

    update base url

--- src/app/constant/constants.js
+++ src/app/constant/constants.js
@@ -4,8 +4,8 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 export const BASE_URL = isProduction
   ? 'https://bbitapi.ramsbd.net/'
+  : 'http://192.168.68.137:8006';
   : 'https://bbitapi.ramsbd.net';
-// : 'http://192.168.68.137:8006';
 
 // added by live api
 
