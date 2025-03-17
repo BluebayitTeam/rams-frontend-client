52cbd4da7cd76b19f2ea72bbe64a4e6431d1e9b4
shuvo794 <shuvohossain794@gmail.com>
2025-03-17 10:14:41 +0600

    CallingEntry && demand standardize string quotes and enhance form validation schema

--- src/app/main/rams/CallingEntrysManagement/callingEntry/CallingEntry.jsx
+++ src/app/main/rams/CallingEntrysManagement/callingEntry/CallingEntry.jsx
@@ -1,31 +1,43 @@
-import FuseLoading from '@fuse/core/FuseLoading';
-import FusePageCarded from '@fuse/core/FusePageCarded';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { FormProvider, useForm } from 'react-hook-form';
-import useThemeMediaQuery from '@fuse/hooks/useThemeMediaQuery';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { z } from 'zod';
-import CallingEntryHeader from './CallingEntryHeader';
-import CallingEntryModel from './models/CallingEntryModel';
-import { useGetCallingEntryQuery } from '../CallingEntrysApi';
-import CallingEntryForm from './CallingEntryForm';
-import { hasPermission } from 'src/app/constant/permission/permissionList';
+import FuseLoading from "@fuse/core/FuseLoading";
+import FusePageCarded from "@fuse/core/FusePageCarded";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { FormProvider, useForm } from "react-hook-form";
+import useThemeMediaQuery from "@fuse/hooks/useThemeMediaQuery";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import CallingEntryHeader from "./CallingEntryHeader";
+import CallingEntryModel from "./models/CallingEntryModel";
+import { useGetCallingEntryQuery } from "../CallingEntrysApi";
+import CallingEntryForm from "./CallingEntryForm";
+import { hasPermission } from "src/app/constant/permission/permissionList";
 /**
  * Form Validation Schema
  */
 const schema = z.object({
   sponsor_id_no: z
     .string()
-    .nonempty('You must enter a callingEntry name')
-    .min(5, 'The callingEntry name must be at least 5 characters'),
+    .nonempty("You must enter a callingEntry name")
+    .min(5, "The callingEntry name must be at least 5 characters"),
+  country: z
+    .number()
+    .nonempty("You must enter a callingEntry name")
+    .min(5, "The callingEntry name must be at least 5 characters"),
+  visa_agent: z
+    .number()
+    .nonempty("You must enter a visa agent name")
+    .min(5, "The callingEntry name must be at least 5 characters"),
+  visa_number: z
+    .string()
+    .nonempty("You must enter a visa agent name")
+    .min(5, "The callingEntry name must be at least 5 characters"),
 });
 
 function CallingEntry() {
-  const isMobile = useThemeMediaQuery((theme) => theme.breakpoints.down('lg'));
+  const isMobile = useThemeMediaQuery((theme) => theme.breakpoints.down("lg"));
   const routeParams = useParams();
   const { callingEntryId } = routeParams;
 
@@ -34,22 +46,22 @@ function CallingEntry() {
     isLoading,
     isError,
   } = useGetCallingEntryQuery(callingEntryId, {
-    skip: !callingEntryId || callingEntryId === 'new',
+    skip: !callingEntryId || callingEntryId === "new",
   });
 
   const [tabValue, setTabValue] = useState(0);
 
-  console.log('tabValue', tabValue);
+  console.log("tabValue", tabValue);
 
   const methods = useForm({
-    mode: 'onChange',
+    mode: "onChange",
     defaultValues: {},
     resolver: zodResolver(schema),
   });
   const { reset, watch } = methods;
   const form = watch();
   useEffect(() => {
-    if (callingEntryId === 'new') {
+    if (callingEntryId === "new") {
       reset(CallingEntryModel({}));
     }
   }, [callingEntryId, reset]);
@@ -71,21 +83,23 @@ function CallingEntry() {
   /**
    * Show Message if the requested callingEntry is not exists
    */
-  if (isError && callingEntryId !== 'new') {
+  if (isError && callingEntryId !== "new") {
     return (
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1, transition: { delay: 0.1 } }}
-        className='flex flex-col flex-1 items-center justify-center h-full'>
-        <Typography color='text.secondary' variant='h5'>
+        className="flex flex-col flex-1 items-center justify-center h-full"
+      >
+        <Typography color="text.secondary" variant="h5">
           There is no such callingEntry!
         </Typography>
         <Button
-          className='mt-24'
+          className="mt-24"
           component={Link}
-          variant='outlined'
-          to='/apps/callingEntry/callingEntry'
-          color='inherit'>
+          variant="outlined"
+          to="/apps/callingEntry/callingEntry"
+          color="inherit"
+        >
           Go to CallingEntrys Page
         </Button>
       </motion.div>
@@ -94,15 +108,15 @@ function CallingEntry() {
 
   return (
     <FormProvider {...methods}>
-      {hasPermission('CALLING_ENTRY_DETAILS') && (
+      {hasPermission("CALLING_ENTRY_DETAILS") && (
         <FusePageCarded
           classes={{
-            toolbar: 'p-0',
-            header: 'min-h-80 h-80',
+            toolbar: "p-0",
+            header: "min-h-80 h-80",
           }}
           header={<CallingEntryHeader />}
           content={
-            <div className='p-16 '>
+            <div className="p-16 ">
               <CallingEntryForm callingEntryId={callingEntryId} />
             </div>
           }
