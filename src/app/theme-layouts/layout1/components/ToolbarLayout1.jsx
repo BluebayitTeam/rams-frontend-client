ea84e4126512f472a44eeae5ce62bfaa0b623009
shuvo794 <shuvohossain794@gmail.com>
2025-03-17 15:12:22 +0600

    work a them colour

--- src/app/theme-layouts/layout1/components/ToolbarLayout1.jsx
+++ src/app/theme-layouts/layout1/components/ToolbarLayout1.jsx
@@ -1,24 +1,24 @@
-import { ThemeProvider } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Hidden from '@mui/material/Hidden';
-import Toolbar from '@mui/material/Toolbar';
-import clsx from 'clsx';
-import { memo } from 'react';
-import { useSelector } from 'react-redux';
+import { ThemeProvider } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Hidden from "@mui/material/Hidden";
+import Toolbar from "@mui/material/Toolbar";
+import clsx from "clsx";
+import { memo } from "react";
+import { useSelector } from "react-redux";
 import {
   selectFuseCurrentLayoutConfig,
   selectToolbarTheme,
-} from '@fuse/core/FuseSettings/store/fuseSettingsSlice';
-import NotificationPanelToggleButton from 'src/app/main/apps/notifications/NotificationPanelToggleButton';
-import NavbarToggleButton from 'app/theme-layouts/shared-components/navbar/NavbarToggleButton';
-import { selectFuseNavbar } from 'app/theme-layouts/shared-components/navbar/store/navbarSlice';
-import AdjustFontSize from '../../shared-components/AdjustFontSize';
-import FullScreenToggle from '../../shared-components/FullScreenToggle';
-import LanguageSwitcher from '../../shared-components/LanguageSwitcher';
-import NavigationShortcuts from '../../shared-components/navigation/NavigationShortcuts';
-import NavigationSearch from '../../shared-components/navigation/NavigationSearch';
-import UserMenu from '../../shared-components/UserMenu';
-import QuickPanelToggleButton from '../../shared-components/quickPanel/QuickPanelToggleButton';
+} from "@fuse/core/FuseSettings/store/fuseSettingsSlice";
+import NotificationPanelToggleButton from "src/app/main/apps/notifications/NotificationPanelToggleButton";
+import NavbarToggleButton from "app/theme-layouts/shared-components/navbar/NavbarToggleButton";
+import { selectFuseNavbar } from "app/theme-layouts/shared-components/navbar/store/navbarSlice";
+import AdjustFontSize from "../../shared-components/AdjustFontSize";
+import FullScreenToggle from "../../shared-components/FullScreenToggle";
+import NavigationShortcuts from "../../shared-components/navigation/NavigationShortcuts";
+import NavigationSearch from "../../shared-components/navigation/NavigationSearch";
+import UserMenu from "../../shared-components/UserMenu";
+import QuickPanelToggleButton from "../../shared-components/quickPanel/QuickPanelToggleButton";
+import Configurator from "app/theme-layouts/shared-components/configurator/Configurator";
 
 /**
  * The toolbar layout 1.
@@ -31,34 +31,35 @@ function ToolbarLayout1(props) {
   return (
     <ThemeProvider theme={toolbarTheme}>
       <AppBar
-        id='fuse-toolbar'
-        className={clsx('relative z-20 flex shadow', className)}
-        color='default'
+        id="fuse-toolbar"
+        className={clsx("relative z-20 flex shadow", className)}
+        color="default"
         sx={{
           backgroundColor: (theme) =>
-            theme.palette.mode === 'light'
+            theme.palette.mode === "light"
               ? toolbarTheme.palette.background.paper
               : toolbarTheme.palette.background.default,
         }}
-        position='static'
-        elevation={0}>
-        <Toolbar className='min-h-48 p-0 md:min-h-64'>
-          <div className='flex flex-1 px-16'>
-            {config.navbar.display && config.navbar.position === 'left' && (
+        position="static"
+        elevation={0}
+      >
+        <Toolbar className="min-h-48 p-0 md:min-h-64">
+          <div className="flex flex-1 px-16">
+            {config.navbar.display && config.navbar.position === "left" && (
               <>
                 <Hidden lgDown>
-                  {(config.navbar.style === 'style-3' ||
-                    config.navbar.style === 'style-3-dense') && (
-                    <NavbarToggleButton className='mx-0 h-40 w-40 p-0' />
+                  {(config.navbar.style === "style-3" ||
+                    config.navbar.style === "style-3-dense") && (
+                    <NavbarToggleButton className="mx-0 h-40 w-40 p-0" />
                   )}
 
-                  {config.navbar.style === 'style-1' && !navbar.open && (
-                    <NavbarToggleButton className='mx-0 h-40 w-40 p-0' />
+                  {config.navbar.style === "style-1" && !navbar.open && (
+                    <NavbarToggleButton className="mx-0 h-40 w-40 p-0" />
                   )}
                 </Hidden>
 
                 <Hidden lgUp>
-                  <NavbarToggleButton className='mx-0 h-40 w-40 p-0 sm:mx-8' />
+                  <NavbarToggleButton className="mx-0 h-40 w-40 p-0 sm:mx-8" />
                 </Hidden>
               </>
             )}
@@ -68,8 +69,9 @@ function ToolbarLayout1(props) {
             </Hidden>
           </div>
 
-          <div className='flex h-full items-center overflow-x-auto px-8'>
+          <div className="flex h-full items-center overflow-x-auto px-8">
             {/* <LanguageSwitcher /> */}
+            <Configurator />
             <AdjustFontSize />
             <FullScreenToggle />
             <NavigationSearch />
@@ -78,16 +80,16 @@ function ToolbarLayout1(props) {
             <UserMenu />
           </div>
 
-          {config.navbar.display && config.navbar.position === 'right' && (
+          {config.navbar.display && config.navbar.position === "right" && (
             <>
               <Hidden lgDown>
                 {!navbar.open && (
-                  <NavbarToggleButton className='mx-0 h-40 w-40 p-0' />
+                  <NavbarToggleButton className="mx-0 h-40 w-40 p-0" />
                 )}
               </Hidden>
 
               <Hidden lgUp>
-                <NavbarToggleButton className='mx-0 h-40 w-40 p-0 sm:mx-8' />
+                <NavbarToggleButton className="mx-0 h-40 w-40 p-0 sm:mx-8" />
               </Hidden>
             </>
           )}
