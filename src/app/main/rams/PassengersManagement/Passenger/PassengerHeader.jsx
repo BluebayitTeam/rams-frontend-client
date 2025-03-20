89aca1604bffec740d6813072c3ec4917f560fd0
shuvo794 <shuvohossain794@gmail.com>
2025-03-20 14:39:56 +0600

    fix: update BASE_URL for development environment and improve code formatting in PassengerHeader component

--- src/app/main/rams/PassengersManagement/Passenger/PassengerHeader.jsx
+++ src/app/main/rams/PassengersManagement/Passenger/PassengerHeader.jsx
@@ -1,33 +1,34 @@
 /* eslint-disable no-undef */
-import Button from '@mui/material/Button';
-import { useTheme } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
-import { motion } from 'framer-motion';
-import { useFormContext } from 'react-hook-form';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import _ from '@lodash';
-import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
-import { Icon } from '@mui/material';
-import { showMessage } from '@fuse/core/FuseMessage/store/fuseMessageSlice';
+import Button from "@mui/material/Button";
+import { useTheme } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import { motion } from "framer-motion";
+import { useFormContext } from "react-hook-form";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import _ from "@lodash";
+import FuseSvgIcon from "@fuse/core/FuseSvgIcon";
+import { Icon } from "@mui/material";
+import { showMessage } from "@fuse/core/FuseMessage/store/fuseMessageSlice";
 import {
   AddedSuccessfully,
   DeletedSuccessfully,
   UpdatedSuccessfully,
-} from 'src/app/@customHooks/notificationAlert';
+} from "src/app/@customHooks/notificationAlert";
 import {
   useCreatePassengerMutation,
   useDeletePassengerMutation,
   useUpdatePassengerMutation,
-} from '../PassengersApi';
-import { hasPermission } from 'src/app/constant/permission/permissionList';
-import { BASE_URL } from 'src/app/constant/constants';
-import { useGetAllNotificationsQuery } from 'src/app/main/apps/notifications/NotificationApi';
+} from "../PassengersApi";
+import { hasPermission } from "src/app/constant/permission/permissionList";
+import { BASE_URL } from "src/app/constant/constants";
+import { useGetAllNotificationsQuery } from "src/app/main/apps/notifications/NotificationApi";
 
 /**
  * The passenger header.
  */
 function PassengerHeader() {
   const routeParams = useParams();
+  console.log("routeParams", routeParams);
   const { passengerId, passengerType } = routeParams;
 
   const [createPassenger] = useCreatePassengerMutation();
@@ -41,15 +42,15 @@ function PassengerHeader() {
   const theme = useTheme();
   const navigate = useNavigate();
   const { name, images, passenger_pic, featuredImageId } = watch();
-  const handleDelete = localStorage.getItem('deletePassenger');
-  const handleUpdate = localStorage.getItem('updatePassenger');
+  const handleDelete = localStorage.getItem("deletePassenger");
+  const handleUpdate = localStorage.getItem("updatePassenger");
 
   const { passengerName, fromSearch } = useParams();
 
   function handleUpdatePassenger() {
     savePassenger(getValues()).then((data) => {
       UpdatedSuccessfully();
-      if (passengerType == 'fromSearch') {
+      if (passengerType == "fromSearch") {
         navigate(-1);
       } else {
         refetch();
@@ -74,12 +75,12 @@ function PassengerHeader() {
     DeletedSuccessfully();
     navigate(`/apps/passenger/passengers/${routeParams?.passengerType}`);
     dispatch(
-      showMessage({ message: `Please Restart The Backend`, variant: 'error' })
+      showMessage({ message: `Please Restart The Backend`, variant: "error" })
     );
   }
 
   function handleCancel() {
-    if (passengerType == 'fromSearch') {
+    if (passengerType == "fromSearch") {
       navigate(-1);
     } else {
       navigate(`/apps/passenger/passengers/${routeParams?.passengerType}`);
@@ -87,16 +88,17 @@ function PassengerHeader() {
   }
 
   return (
-    <div className='flex flex-col sm:flex-row flex-1 w-full items-center justify-between space-y-8 sm:space-y-0 py-24 sm:py-32 px-24 md:px-32'>
-      <div className='flex flex-col items-start space-y-8 sm:space-y-0 w-2/3 sm:max-w-full min-w-0'>
+    <div className="flex flex-col sm:flex-row flex-1 w-full items-center justify-between space-y-8 sm:space-y-0 py-24 sm:py-32 px-24 md:px-32">
+      <div className="flex flex-col items-start space-y-8 sm:space-y-0 w-2/3 sm:max-w-full min-w-0">
         <motion.div
           initial={{ x: 20, opacity: 0 }}
-          animate={{ x: 0, opacity: 1, transition: { delay: 0.3 } }}>
+          animate={{ x: 0, opacity: 1, transition: { delay: 0.3 } }}
+        >
           <Typography
-            className='flex items-center sm:mb-12'
-            role='button'
+            className="flex items-center sm:mb-12"
+            role="button"
             onClick={() => {
-              if (passengerType === 'fromSearch') {
+              if (passengerType === "fromSearch") {
                 navigate(-1);
               } else {
                 navigate(
@@ -104,50 +106,52 @@ function PassengerHeader() {
                 );
               }
             }}
-            color='inherit'
-            style={{ cursor: 'pointer' }} // Ensures it's clickable
+            color="inherit"
+            style={{ cursor: "pointer" }} // Ensures it's clickable
           >
             <FuseSvgIcon size={20}>
-              {theme.direction === 'ltr'
-                ? 'heroicons-outline:arrow-sm-left'
-                : 'heroicons-outline:arrow-sm-right'}
+              {theme.direction === "ltr"
+                ? "heroicons-outline:arrow-sm-left"
+                : "heroicons-outline:arrow-sm-right"}
             </FuseSvgIcon>
-            <span className='flex mx-4 font-medium'>Passengers</span>
+            <span className="flex mx-4 font-medium">Passengers</span>
           </Typography>
         </motion.div>
 
-        <div className='flex items-center max-w-full'>
+        <div className="flex items-center max-w-full">
           <motion.div
-            className='hidden sm:flex'
+            className="hidden sm:flex"
             initial={{ scale: 0 }}
-            animate={{ scale: 1, transition: { delay: 0.3 } }}>
+            animate={{ scale: 1, transition: { delay: 0.3 } }}
+          >
             {passenger_pic && passenger_pic.length > 0 ? (
               <img
-                className='w-32 sm:w-48 rounded'
+                className="w-32 sm:w-48 rounded"
                 style={{
-                  height: '60px',
-                  width: '60px',
-                  borderRadius: '50%',
+                  height: "60px",
+                  width: "60px",
+                  borderRadius: "50%",
                 }}
                 src={`${BASE_URL}${passenger_pic}`}
                 alt={name}
               />
             ) : (
               <img
-                className='w-32 sm:w-48 rounded'
-                src='/assets/images/logos/user.jpg'
+                className="w-32 sm:w-48 rounded"
+                src="/assets/images/logos/user.jpg"
                 alt={name}
               />
             )}
           </motion.div>
           <motion.div
-            className='flex flex-col min-w-0 mx-8 sm:mx-16'
+            className="flex flex-col min-w-0 mx-8 sm:mx-16"
             initial={{ x: -20 }}
-            animate={{ x: 0, transition: { delay: 0.3 } }}>
-            <Typography className='text-16 sm:text-20 truncate font-semibold'>
-              {name || 'New Passenger'}
+            animate={{ x: 0, transition: { delay: 0.3 } }}
+          >
+            <Typography className="text-16 sm:text-20 truncate font-semibold">
+              {name || "New Passenger"}
             </Typography>
-            <Typography variant='caption' className='font-medium'>
+            <Typography variant="caption" className="font-medium">
               Passenger Detail
             </Typography>
           </motion.div>
@@ -155,52 +159,53 @@ function PassengerHeader() {
       </div>
 
       <motion.div
-        className='flex'
+        className="flex"
         initial={{ opacity: 0, x: 20 }}
-        animate={{ opacity: 1, x: 0, transition: { delay: 0.3 } }}>
-        {handleDelete === 'deletePassenger' && passengerId !== 'new' && (
-          <Typography className='mt-6' variant='subtitle2'>
+        animate={{ opacity: 1, x: 0, transition: { delay: 0.3 } }}
+      >
+        {handleDelete === "deletePassenger" && passengerId !== "new" && (
+          <Typography className="mt-6" variant="subtitle2">
             Do you want to remove this passenger?
           </Typography>
         )}
-        {handleDelete === 'deletePassenger' &&
-          passengerId !== 'new' &&
-          hasPermission('PASSENGER_DELETE') && (
-            <Button
-              className='whitespace-nowrap mx-2 text-white bg-red-400 hover:bg-red-800 active:bg-red-700 focus:outline-none focus:ring focus:ring-[#ea5b78]-300'
-              variant='contained'
-              style={{ padding: '0 28px' }}
-              onClick={handleRemovePassenger}
-              startIcon={<Icon className='hidden sm:flex'>delete</Icon>}>
-              Remove
-            </Button>
-          )}
-        {passengerId === 'new' && hasPermission('PASSENGER_CREATE') && (
+        {(passengerType === "fromSearch" ||
+          (handleDelete === "deletePassenger" && passengerId !== "new")) && (
           <Button
-            className='whitespace-nowrap mx-2'
-            variant='contained'
-            color='secondary'
+            className="whitespace-nowrap mx-2 text-white bg-red-400 hover:bg-red-800 active:bg-red-700 focus:outline-none focus:ring focus:ring-[#ea5b78]-300"
+            variant="contained"
+            style={{ padding: "0 28px" }}
+            onClick={handleRemovePassenger}
+            startIcon={<Icon className="hidden sm:flex">delete</Icon>}
+          >
+            Remove
+          </Button>
+        )}
+        {passengerId === "new" && (
+          <Button
+            className="whitespace-nowrap mx-2"
+            variant="contained"
+            color="secondary"
             disabled={_.isEmpty(dirtyFields) || !isValid}
-            onClick={handleCreatePassenger}>
+            onClick={handleCreatePassenger}
+          >
             Save
           </Button>
         )}
-        {handleDelete !== 'deletePassenger' &&
-          handleUpdate === 'updatePassenger' &&
-          passengerId !== 'new' &&
-          hasPermission('PASSENGER_UPDATE') && (
-            <Button
-              className='whitespace-nowrap mx-4 text-white bg-green-500 hover:bg-green-800 active:bg-green-700 focus:outline-none focus:ring focus:ring-green-300'
-              color='secondary'
-              variant='contained'
-              onClick={handleUpdatePassenger}>
-              Update
-            </Button>
-          )}
+        {handleDelete !== "deletePassenger" && passengerType && (
+          <Button
+            className="whitespace-nowrap mx-4 text-white bg-green-500 hover:bg-green-800 active:bg-green-700 focus:outline-none focus:ring focus:ring-green-300"
+            color="secondary"
+            variant="contained"
+            onClick={handleUpdatePassenger}
+          >
+            Update
+          </Button>
+        )}
         <Button
-          className='whitespace-nowrap mx-2 text-white bg-orange-500 hover:bg-orange-800 active:bg-orange-700 focus:outline-none focus:ring focus:ring-orange-300'
-          variant='contained'
-          onClick={handleCancel}>
+          className="whitespace-nowrap mx-2 text-white bg-orange-500 hover:bg-orange-800 active:bg-orange-700 focus:outline-none focus:ring focus:ring-orange-300"
+          variant="contained"
+          onClick={handleCancel}
+        >
           Cancel
         </Button>
       </motion.div>
